5cd1ed0a0e888172ecd035e73fa9cf8914e8d5fd
rommuluslewis <rommuluslewis@gmail.com>
2025-02-12 01:14:11 +0800

    my plotting code fix + c_sie and nfw runs

--- C_SIE/pos_point.py
+++ C_SIE/pos_point.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python
 import glafic
 
-glafic.init(0.3, 0.7, -1.0, 0.7, 'C_SIE/SIE_POS', 0.0, 0.0, 5.0, 5.0, 0.04, 0.04, 1, verb = 0)
+glafic.init(0.3, 0.7, -1.0, 0.7, 'C_SIE/SIE_POS', 20.0, 20.0, 21.56, 21.56, 0.01, 0.01, 1, verb = 0)
 
 glafic.set_secondary('chi2_splane 0', verb = 0)
 glafic.set_secondary('chi2_checknimg 0', verb = 0)
 glafic.set_secondary('chi2_restart   -1', verb = 0)
-glafic.set_secondary('chi2_usemag    0', verb = 0)
+glafic.set_secondary('chi2_usemag    1', verb = 0)
 glafic.set_secondary('hvary          0', verb = 0)
 glafic.set_secondary('ran_seed -122000', verb = 0)
 
 glafic.startup_setnum(1, 0, 1)
-glafic.set_lens(1, 'sie', 0.261343256161012, 5.0e+02,  2.5, 2.5, 0.4, -4.0, 0.4, 0.0)
-glafic.set_point(1, 1.0, 2.5, 2.5)
+glafic.set_lens(1, 'sie', 0.261343256161012, 1.9e+02,  20.78, 20.78, 0.1, -4.0, 0.01, 0.0)
+glafic.set_point(1, 1.0, 20.78, 20.78)
 
 glafic.setopt_lens(1, 0, 1, 1, 1, 1, 1, 1, 0)
 glafic.setopt_point(1, 0, 1, 1)
