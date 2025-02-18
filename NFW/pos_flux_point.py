47e9238e32adc2b067e1d47e61d20f42f1d944cc
rommuluslewis <rommuluslewis@gmail.com>
2025-02-18 20:50:39 +0800

    nfw run

--- NFW/pos_flux_point.py
+++ NFW/pos_flux_point.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python
 import glafic
 
-glafic.init(0.3, 0.7, -1.0, 0.7, 'NFW/NFW_POS+FLUX', 0.0, 0.0, 41.56, 41.56, 0.04, 0.04, 1, verb = 0)
+glafic.init(0.3, 0.7, -1.0, 0.7, 'NFW/NFW_POS+FLUX', 20.0, 20.0, 21.56, 21.56, 0.01, 0.01, 1, verb = 0)
 
 glafic.set_secondary('chi2_splane 0', verb = 0)
 glafic.set_secondary('chi2_checknimg 0', verb = 0)
 glafic.set_secondary('chi2_restart   -1', verb = 0)
-glafic.set_secondary('chi2_usemag    0', verb = 0)
+glafic.set_secondary('chi2_usemag    1', verb = 0)
 glafic.set_secondary('hvary          0', verb = 0)
 glafic.set_secondary('ran_seed -122000', verb = 0)
 glafic.set_secondary('flag_hodensity 2', verb = 0)
 
 glafic.startup_setnum(1, 0, 1)
-glafic.set_lens(1, 'anfw', 0.261343256161012, 1e+12, 20.78, 20.78, 4.0e-01, 0, 30.0, 0.0)
+glafic.set_lens(1, 'anfw', 0.261343256161012, 1e+12, 20.78, 20.78, 0.2, -4.0, 85.0, 0.0)
 glafic.set_point(1, 1.0, 20.78, 20.78)
 
 glafic.setopt_lens(1, 0, 1, 1, 1, 1, 1, 1, 0)
