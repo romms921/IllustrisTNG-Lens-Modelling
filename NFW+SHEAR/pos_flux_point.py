87d8ed4b0335943720ef74a47024d51627b21ba0
rommuluslewis <rommuluslewis@gmail.com>
2025-02-20 22:35:38 +0800

    nfw run

--- NFW+SHEAR/pos_flux_point.py
+++ NFW+SHEAR/pos_flux_point.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import glafic
 
-glafic.init(0.3, 0.7, -1.0, 0.7, 'POW+SHEAR/POW_POS_SHEAR', 20.0, 20.0, 21.56, 21.56, 0.01, 0.01, 1, verb = 0)
+glafic.init(0.3, 0.7, -1.0, 0.7, 'NFW/NFW_POS+FLUX', 20.0, 20.0, 21.56, 21.56, 0.01, 0.01, 1, verb = 0)
 
 glafic.set_secondary('chi2_splane 0', verb = 0)
 glafic.set_secondary('chi2_checknimg 0', verb = 0)
@@ -9,20 +9,19 @@ glafic.set_secondary('chi2_restart   -1', verb = 0)
 glafic.set_secondary('chi2_usemag    1', verb = 0)
 glafic.set_secondary('hvary          0', verb = 0)
 glafic.set_secondary('ran_seed -122000', verb = 0)
+glafic.set_secondary('flag_hodensity 2', verb = 0)
 
-glafic.startup_setnum(2, 0, 1)
-glafic.set_lens(1, 'pow', 0.261343256161012, 1.0, 20.81, 20.76, 0.12, -4.0, 0.4, 1.5)
-glafic.set_lens(2, 'pert', 0.261343256161012, 1.0, 20.90, 20.64, 0.1, 140.0, 0.0, 0.3)
+glafic.startup_setnum(1, 0, 1)
+glafic.set_lens(1, 'anfw', 0.261343256161012, 1e+12, 20.78, 20.78, 0.2, -4.0, 85.0, 0.0)
 glafic.set_point(1, 1.0, 20.78, 20.78)
 
-glafic.setopt_lens(1, 0, 0, 0, 0, 1, 1, 1, 1)
-glafic.setopt_lens(2, 0, 0, 1, 1, 1, 1, 0, 1)
+glafic.setopt_lens(1, 0, 1, 1, 1, 1, 1, 1, 0)
 glafic.setopt_point(1, 0, 1, 1)
 
 # model_init needs to be done again whenever model parameters are changed
 glafic.model_init(verb = 0)
 
-glafic.readobs_point('/Users/ainsleylewis/Documents/Astronomy/IllustrisTNG Lens Modelling/obs_point/obs_point_(POS).dat')
+glafic.readobs_point('/Users/ainsleylewis/Documents/Astronomy/IllustrisTNG Lens Modelling/obs_point/obs_point_(POS+FLUX).dat')
 glafic.optimize()
 glafic.findimg()
 glafic.writecrit(1.0)
