d6b650f2ed4c8e4bea528000ded3462289539463
rommuluslewis <rommuluslewis@gmail.com>
2025-02-26 21:09:55 +0800

    ein pos + flux run

--- EIN/pos_flux_point.py
+++ EIN/pos_flux_point.py
@@ -12,7 +12,7 @@ glafic.set_secondary('ran_seed -122000', verb = 0)
 glafic.set_secondary('flag_hodensity 2', verb = 0)
 
 glafic.startup_setnum(1, 0, 1)
-glafic.set_lens(1, 'ein', 0.261343256161012, 1.29e+11, 20.81, 20.76, 0.1, -4.46, 35.0, 0.5)
+glafic.set_lens(1, 'ein', 0.261343256161012, 1.29e+11, 20.81, 20.76, 0.1, -4.46, 50.0, 0.5)
 glafic.set_point(1, 1.0, 20.78, 20.78)
 
 glafic.setopt_lens(1, 0, 1, 1, 1, 1, 1, 1, 0)
