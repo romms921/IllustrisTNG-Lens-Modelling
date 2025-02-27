54c7649c14450c90ea206340839c582a55e2e619
rommuluslewis <rommuluslewis@gmail.com>
2025-02-27 10:46:40 +0800

    ein pos flux shear fix run

--- EIN+SHEAR+FIX/pos_point.py
+++ EIN+SHEAR+FIX/pos_point.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import glafic
 
-glafic.init(0.3, 0.7, -1.0, 0.7, 'EIN+SHEAR+FIX/EIN_POS+FLUX_SHEAR', 20.0, 20.0, 21.56, 21.56, 0.01, 0.01, 1, verb = 0)
+glafic.init(0.3, 0.7, -1.0, 0.7, 'EIN+SHEAR+FIX/EIN_POS_SHEAR', 20.0, 20.0, 21.56, 21.56, 0.01, 0.01, 1, verb = 0)
 
 glafic.set_secondary('chi2_splane 1', verb = 0)
 glafic.set_secondary('chi2_checknimg 0', verb = 0)
@@ -23,7 +23,7 @@ glafic.setopt_point(1, 0, 1, 1)
 # model_init needs to be done again whenever model parameters are changed
 glafic.model_init(verb = 0)
 
-glafic.readobs_point('/Users/ainsleylewis/Documents/Astronomy/IllustrisTNG Lens Modelling/obs_point/obs_point_(POS+FLUX).dat')
+glafic.readobs_point('/Users/ainsleylewis/Documents/Astronomy/IllustrisTNG Lens Modelling/obs_point/obs_point_(POS).dat')
 glafic.optimize()
 glafic.findimg()
 glafic.writecrit(1.0)
