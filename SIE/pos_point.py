53aee37f9efddebc0536ab3edac3c964bbe82b11
rommuluslewis <rommuluslewis@gmail.com>
2025-03-01 11:06:47 +0800

    sie pos run 0.001

--- SIE/pos_point.py
+++ SIE/pos_point.py
@@ -11,7 +11,7 @@ glafic.set_secondary('hvary          0', verb = 0)
 glafic.set_secondary('ran_seed -122000', verb = 0)
 
 glafic.startup_setnum(1, 0, 1)
-glafic.set_lens(1, 'sie', 0.261343256161012, 1.58e+02, 20.80, 20.75, 0.12, 175.0, 0.0, 0.0)
+glafic.set_lens(1, 'sie', 0.261343256161012, 1.58e+02, 20.80, 20.75, 0.12, -4.0, 0.0, 0.0)
 glafic.set_point(1, 1.0, 20.78, 20.78)
 
 glafic.setopt_lens(1, 0, 1, 1, 1, 1, 1, 0, 0)
