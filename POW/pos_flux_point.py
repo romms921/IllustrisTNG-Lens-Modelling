587805d92d06d3529e8e1717148a4dca715788d9
rommuluslewis <rommuluslewis@gmail.com>
2025-03-02 14:12:46 +0800

    pow pos+flux update

--- POW/pos_flux_point.py
+++ POW/pos_flux_point.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import glafic
 
-glafic.init(0.3, 0.7, -1.0, 0.7, 'POW/POW_POS+FLUX', 20.0, 20.0, 21.56, 21.56, 0.01, 0.01, 1, verb = 0)
+glafic.init(0.3, 0.7, -1.0, 0.7, 'POW/POW_POS+FLUX', 20.0, 20.0, 21.56, 21.56, 0.001, 0.001, 1, verb = 0)
 
 glafic.set_secondary('chi2_splane 0', verb = 0)
 glafic.set_secondary('chi2_checknimg 0', verb = 0)
@@ -12,7 +12,7 @@ glafic.set_secondary('ran_seed -122000', verb = 0)
 
 glafic.startup_setnum(1, 0, 1)
 # pow index is 2.1 for POS constraint
-glafic.set_lens(1, 'pow', 0.261343256161012, 1.0, 20.78, 20.78, 0.12, -4.0, 0.4, 2.0) 
+glafic.set_lens(1, 'pow', 0.261343256161012, 1.0, 20.81, 20.76, 0.12, -4.4, 0.49, 1.73) 
 glafic.set_point(1, 1.0, 20.78, 20.78)
 
 glafic.setopt_lens(1, 0, 0, 1, 1, 1, 1, 1, 1)
