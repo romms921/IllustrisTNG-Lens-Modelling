9280a2422c98aee6b5ef3d1c34645997d8cf8eab
rommuluslewis <rommuluslewis@gmail.com>
2025-03-05 13:52:03 +0800

    ein pos+flux run 0.001

--- EIN/pos_flux_point.py
+++ EIN/pos_flux_point.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import glafic
 
-glafic.init(0.3, 0.7, -1.0, 0.7, 'EIN/EIN_POS+FLUX', 20.0, 20.0, 21.56, 21.56, 0.01, 0.01, 1, verb = 0)
+glafic.init(0.3, 0.7, -1.0, 0.7, 'EIN/EIN_POS+FLUX', 20.0, 20.0, 21.56, 21.56, 0.001, 0.001, 1, verb = 0)
 
 glafic.set_secondary('chi2_splane 0', verb = 0)
 glafic.set_secondary('chi2_checknimg 0', verb = 0)
@@ -12,7 +12,7 @@ glafic.set_secondary('ran_seed -122000', verb = 0)
 glafic.set_secondary('flag_hodensity 2', verb = 0)
 
 glafic.startup_setnum(1, 0, 1)
-glafic.set_lens(1, 'ein', 0.261343256161012, 1.29e+11, 20.81, 20.76, 0.1, -4.46, 50.0, 0.5)
+glafic.set_lens(1, 'ein', 0.261343256161012, 6.52e+10, 20.80, 20.75, 0.14, -4.46, 50.0, 0.5)
 glafic.set_point(1, 1.0, 20.78, 20.78)
 
 glafic.setopt_lens(1, 0, 1, 1, 1, 1, 1, 1, 0)
