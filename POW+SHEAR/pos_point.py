d9007e8c1a6e6aaf013ec65862f054debaefc4dc
rommuluslewis <rommuluslewis@gmail.com>
2025-03-06 21:19:49 +0800

    pow pos run 0.001

--- POW+SHEAR/pos_point.py
+++ POW+SHEAR/pos_point.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import glafic
 
-glafic.init(0.3, 0.7, -1.0, 0.7, 'POW+SHEAR/POW_POS_SHEAR', 20.0, 20.0, 21.56, 21.56, 0.01, 0.01, 1, verb = 0)
+glafic.init(0.3, 0.7, -1.0, 0.7, 'POW+SHEAR/POW_POS_SHEAR', 20.0, 20.0, 21.56, 21.56, 0.001, 0.001, 1, verb = 0)
 
 glafic.set_secondary('chi2_splane 0', verb = 0)
 glafic.set_secondary('chi2_checknimg 0', verb = 0)
@@ -11,11 +11,11 @@ glafic.set_secondary('hvary          0', verb = 0)
 glafic.set_secondary('ran_seed -122000', verb = 0)
 
 glafic.startup_setnum(2, 0, 1)
-glafic.set_lens(1, 'pow', 0.261343256161012, 1.0, 20.81, 20.76, 0.12, -4.0, 0.4, 1.5)
-glafic.set_lens(2, 'pert', 0.261343256161012, 1.0, 20.90, 20.64, 0.1, 140.0, 0.0, 0.3)
+glafic.set_lens(1, 'pow', 0.261343256161012, 1.0, 20.81, 20.76, 0.16, 12.13, 0.34, 2.24)
+glafic.set_lens(2, 'pert', 0.261343256161012, 1.0, 20.88, 20.64, 0.1, 143.0, 0.0, 0.3)
 glafic.set_point(1, 1.0, 20.78, 20.78)
 
-glafic.setopt_lens(1, 0, 0, 0, 0, 1, 1, 1, 1)
+glafic.setopt_lens(1, 0, 0, 0, 0, 1, 1, 1, 0)
 glafic.setopt_lens(2, 0, 0, 1, 1, 1, 1, 0, 1)
 glafic.setopt_point(1, 0, 1, 1)
 
