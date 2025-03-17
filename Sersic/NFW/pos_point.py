d819f58dc4e670a8a6ca0f12f552af7b05b3e76e
rommuluslewis <rommuluslewis@gmail.com>
2025-03-18 00:45:04 +0800

    sersic plotting code fix

--- Sersic/NFW/pos_point.py
+++ Sersic/NFW/pos_point.py
@@ -13,14 +13,14 @@ glafic.set_secondary('flag_hodensity 2', verb = 0)
 
 glafic.startup_setnum(3, 0, 1)
 glafic.set_lens(1, 'anfw', 0.261343256161012, 2.085312e+11, 2.080884e+01, 2.075341e+01, 1.500761e-01, -4.431327e+00,  8.699047e+01, 0.000000e+00)
-glafic.set_lens(2, 'sers', 0.261343256161012, 8e+11, 21.280727179073, 20.8498660122583, 0.19174153447615125, 97.15579684060029, 51.88103345355174, 1.4424355377364537)
-glafic.set_lens(3, 'sers', 0.261343256161012, 1e+11, 21.280727179073, 20.8498660122583, 0.5818587320719051, 80.90881818383842, 5.979267867493113, 0.5737795483133368)
+glafic.set_lens(2, 'sers', 0.261343256161012, 8e+11, 21.2807, 20.8498, 0.19174, 97.1557, 51.8810, 1.4424)
+glafic.set_lens(3, 'sers', 0.261343256161012, 1e+11, 21.2807, 20.8498, 0.58185, 80.9088, 5.97926, 0.5737)
 glafic.set_point(1, 1.0, 2.081271e+01, 2.077975e+01)
 
-glafic.setopt_lens(1, 0, 0, 0, 0, 0, 0, 0, 0)
+glafic.setopt_lens(1, 0, 1, 1, 1, 1, 1, 1, 0)
 glafic.setopt_lens(2, 0, 1, 0, 0, 0, 0, 0, 0)
 glafic.setopt_lens(3, 0, 1, 0, 0, 0, 0, 0, 0)
-glafic.setopt_point(1, 0, 0, 0)
+glafic.setopt_point(1, 0, 1, 1)
 
 # model_init needs to be done again whenever model parameters are changed
 glafic.model_init(verb = 0)
