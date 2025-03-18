45e1af1335e35af22d2f18325af94538eb397f1b
rommuluslewis <rommuluslewis@gmail.com>
2025-03-18 08:21:42 +0800

    added nfw sersic pos+flux

--- Sersic/NFW/pos_flux_point.py
+++ Sersic/NFW/pos_flux_point.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import glafic
 
-glafic.init(0.3, 0.7, -1.0, 0.7, 'NFW/NFW_POS+FLUX', 20.0, 20.0, 21.56, 21.56, 0.001, 0.001, 1, verb = 0)
+glafic.init(0.3, 0.7, -1.0, 0.7, 'Sersic/NFW/NFW_POS+FLUX', 20.0, 20.0, 21.56, 21.56, 0.001, 0.001, 1, verb = 0)
 
 glafic.set_secondary('chi2_splane 0', verb = 0)
 glafic.set_secondary('chi2_checknimg 0', verb = 0)
@@ -11,11 +11,15 @@ glafic.set_secondary('hvary          0', verb = 0)
 glafic.set_secondary('ran_seed -122000', verb = 0)
 glafic.set_secondary('flag_hodensity 2', verb = 0)
 
-glafic.startup_setnum(1, 0, 1)
-glafic.set_lens(1, 'anfw', 0.261343256161012, 2.1e+11, 20.80, 20.75, 0.14, -4.4, 85.0, 0.0)
-glafic.set_point(1, 1.0, 20.78, 20.78)
+glafic.startup_setnum(3, 0, 1)
+glafic.set_lens(1, 'anfw', 0.261343256161012, 2.135149e+11, 2.080906e+01, 2.075443e+01, 1.452121e-01, -4.428302e+00,  8.498801e+01, 0.000000e+00)
+glafic.set_lens(2, 'sers', 0.261343256161012, 8e+10, 21.2807, 20.8498, 0.19174, 97.1557, 51.8810, 1.4424)
+glafic.set_lens(3, 'sers', 0.261343256161012, 1e+10, 21.2807, 20.8498, 0.58185, 80.9088, 5.97926, 0.5737)
+glafic.set_point(1, 1.0, 2.081280e+01, 2.077991e+01)
 
 glafic.setopt_lens(1, 0, 1, 1, 1, 1, 1, 1, 0)
+glafic.setopt_lens(2, 0, 1, 0, 0, 0, 0, 0, 0)
+glafic.setopt_lens(3, 0, 1, 0, 0, 0, 0, 0, 0)
 glafic.setopt_point(1, 0, 1, 1)
 
 # model_init needs to be done again whenever model parameters are changed
