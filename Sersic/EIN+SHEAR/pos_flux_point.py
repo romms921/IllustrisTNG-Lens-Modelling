af5433656187e0319a57443bb7ef3de66f6a375c
rommuluslewis <rommuluslewis@gmail.com>
2025-03-18 19:07:01 +0800

    ein pos sersic fixed shear run

--- Sersic/EIN+SHEAR/pos_flux_point.py
+++ Sersic/EIN+SHEAR/pos_flux_point.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 import glafic
 
-glafic.init(0.3, 0.7, -1.0, 0.7, 'EIN+SHEAR/EIN_POS+FLUX_SHEAR', 20.0, 20.0, 21.56, 21.56, 0.01, 0.01, 1, verb = 0)
+glafic.init(0.3, 0.7, -1.0, 0.7, 'Sersic/EIN+SHEAR/EIN_POS+FLUX_SHEAR', 20.0, 20.0, 21.56, 21.56, 0.01, 0.01, 1, verb = 0)
 
-glafic.set_secondary('chi2_splane 1', verb = 0)
+glafic.set_secondary('chi2_splane 0', verb = 0)
 glafic.set_secondary('chi2_checknimg 0', verb = 0)
 glafic.set_secondary('chi2_restart   -1', verb = 0)
 glafic.set_secondary('chi2_usemag    1', verb = 0)
@@ -11,13 +11,17 @@ glafic.set_secondary('hvary          0', verb = 0)
 glafic.set_secondary('ran_seed -122000', verb = 0)
 glafic.set_secondary('flag_hodensity 2', verb = 0)
 
-glafic.startup_setnum(2, 0, 1)
-glafic.set_lens(1, 'ein', 0.261343256161012, 1.29e+11, 20.81, 20.76, 0.1, -4.46, 50.0, 0.5)
-glafic.set_lens(2, 'pert', 0.261343256161012, 1.0, 20.90, 20.64, 0.1, 140.0, 0.0, 0.00001)
-glafic.set_point(1, 1.0, 20.78, 20.78)
+glafic.startup_setnum(4, 0, 1)
+glafic.set_lens(1, 'ein', 0.261343256161012, 8.908172e+10, 2.081306e+01, 2.076370e+01, 7.509136e-02, 2.244962e+01, 4.995572e+01, 5.000000e-01)
+glafic.set_lens(2, 'sers', 0.261343256161012, 8e+10, 21.2807, 20.8498, 0.19174, 97.1557, 51.8810, 1.4424)
+glafic.set_lens(3, 'sers', 0.261343256161012, 1e+10, 21.2807, 20.8498, 0.58185, 80.9088, 5.97926, 0.5737)
+glafic.set_lens(4, 'pert', 0.261343256161012, 1.0, 2.092145e+01, 2.067114e+01, 3.454152e-02, 1.463911e+02, 0.000000e+00, 1.0e-05)
+glafic.set_point(1, 1.0, 2.079539e+01, 2.080113e+01)
 
 glafic.setopt_lens(1, 0, 1, 1, 1, 1, 1, 1, 0)
-glafic.setopt_lens(2, 0, 0, 1, 1, 1, 1, 0, 1)
+glafic.setopt_lens(2, 0, 1, 0, 0, 0, 0, 0, 0)
+glafic.setopt_lens(3, 0, 1, 0, 0, 0, 0, 0, 0)
+glafic.setopt_lens(4, 0, 0, 1, 1, 1, 1, 0, 1)
 glafic.setopt_point(1, 0, 1, 1)
 
 # model_init needs to be done again whenever model parameters are changed
--- Sersic/EIN+SHEAR/pos_point.py
+++ Sersic/EIN+SHEAR/pos_point.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 import glafic
 
-glafic.init(0.3, 0.7, -1.0, 0.7, 'EIN+SHEAR/EIN_POS_SHEAR', 20.0, 20.0, 21.56, 21.56, 0.01, 0.01, 1, verb = 0)
+glafic.init(0.3, 0.7, -1.0, 0.7, 'Sersic/EIN+SHEAR/EIN_POS_SHEAR', 20.0, 20.0, 21.56, 21.56, 0.01, 0.01, 1, verb = 0)
 
-glafic.set_secondary('chi2_splane 1', verb = 0)
+glafic.set_secondary('chi2_splane 0', verb = 0)
 glafic.set_secondary('chi2_checknimg 0', verb = 0)
 glafic.set_secondary('chi2_restart   -1', verb = 0)
 glafic.set_secondary('chi2_usemag    1', verb = 0)
@@ -11,13 +11,17 @@ glafic.set_secondary('hvary          0', verb = 0)
 glafic.set_secondary('ran_seed -122000', verb = 0)
 glafic.set_secondary('flag_hodensity 2', verb = 0)
 
-glafic.startup_setnum(2, 0, 1)
-glafic.set_lens(1, 'ein', 0.261343256161012, 1.29e+11, 20.81, 20.76, 0.1, -4.46, 50.0, 0.7)
-glafic.set_lens(2, 'pert', 0.261343256161012, 1.0, 20.90, 20.64, 0.1, 140.0, 0.0, 0.00001)
-glafic.set_point(1, 1.0, 20.78, 20.78)
+glafic.startup_setnum(4, 0, 1)
+glafic.set_lens(1, 'ein', 0.261343256161012, 6.352253e+10, 2.081351e+01, 2.076686e+01, 6.427027e-02, 3.412293e+01, 4.997790e+01, 7.000000e-01)
+glafic.set_lens(2, 'sers', 0.261343256161012, 8e+10, 21.2807, 20.8498, 0.19174, 97.1557, 51.8810, 1.4424)
+glafic.set_lens(3, 'sers', 0.261343256161012, 1e+10, 21.2807, 20.8498, 0.58185, 80.9088, 5.97926, 0.5737)
+glafic.set_lens(4, 'pert', 0.261343256161012, 1.000000e+00, 2.087799e+01, 2.063776e+01, 3.954521e-02, 1.513046e+02, 0.000000e+00, 0.000000e+00)
+glafic.set_point(1, 1.0, 2.080713e+01, 2.081149e+01)
 
 glafic.setopt_lens(1, 0, 1, 1, 1, 1, 1, 1, 0)
-glafic.setopt_lens(2, 0, 0, 1, 1, 1, 1, 0, 1)
+glafic.setopt_lens(2, 0, 1, 0, 0, 0, 0, 0, 0)
+glafic.setopt_lens(3, 0, 1, 0, 0, 0, 0, 0, 0)
+glafic.setopt_lens(4, 0, 0, 1, 1, 1, 1, 0, 1)
 glafic.setopt_point(1, 0, 1, 1)
 
 # model_init needs to be done again whenever model parameters are changed
