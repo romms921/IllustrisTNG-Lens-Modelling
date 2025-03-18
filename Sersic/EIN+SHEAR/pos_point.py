51bf40b7ca30b3ea144948147ac4078c29039595
rommuluslewis <rommuluslewis@gmail.com>
2025-03-18 15:30:14 +0800

    added ein+shear+sersic file

---
+++ Sersic/EIN+SHEAR/pos_point.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import glafic
+
+glafic.init(0.3, 0.7, -1.0, 0.7, 'EIN+SHEAR/EIN_POS_SHEAR', 20.0, 20.0, 21.56, 21.56, 0.01, 0.01, 1, verb = 0)
+
+glafic.set_secondary('chi2_splane 1', verb = 0)
+glafic.set_secondary('chi2_checknimg 0', verb = 0)
+glafic.set_secondary('chi2_restart   -1', verb = 0)
+glafic.set_secondary('chi2_usemag    1', verb = 0)
+glafic.set_secondary('hvary          0', verb = 0)
+glafic.set_secondary('ran_seed -122000', verb = 0)
+glafic.set_secondary('flag_hodensity 2', verb = 0)
+
+glafic.startup_setnum(2, 0, 1)
+glafic.set_lens(1, 'ein', 0.261343256161012, 1.29e+11, 20.81, 20.76, 0.1, -4.46, 50.0, 0.7)
+glafic.set_lens(2, 'pert', 0.261343256161012, 1.0, 20.90, 20.64, 0.1, 140.0, 0.0, 0.00001)
+glafic.set_point(1, 1.0, 20.78, 20.78)
+
+glafic.setopt_lens(1, 0, 1, 1, 1, 1, 1, 1, 0)
+glafic.setopt_lens(2, 0, 0, 1, 1, 1, 1, 0, 1)
+glafic.setopt_point(1, 0, 1, 1)
+
+# model_init needs to be done again whenever model parameters are changed
+glafic.model_init(verb = 0)
+
+glafic.readobs_point('/Users/ainsleylewis/Documents/Astronomy/IllustrisTNG Lens Modelling/obs_point/obs_point_(POS).dat')
+glafic.optimize()
+glafic.findimg()
+glafic.writecrit(1.0)
+glafic.writelens(1.0)
+
+glafic.quit()
