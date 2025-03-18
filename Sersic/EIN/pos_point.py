4a1a9ba32cba3660bc5dc56779da45d9a34ecb73
rommuluslewis <rommuluslewis@gmail.com>
2025-03-18 10:43:07 +0800

    ein pos +sersic run

--- Sersic/EIN/pos_point.py
+++ Sersic/EIN/pos_point.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import glafic
 
-glafic.init(0.3, 0.7, -1.0, 0.7, 'EIN/EIN_POS', 20.0, 20.0, 21.56, 21.56, 0.001, 0.001, 1, verb = 0)
+glafic.init(0.3, 0.7, -1.0, 0.7, 'Sersic/EIN/EIN_POS', 20.0, 20.0, 21.56, 21.56, 0.01, 0.01, 1, verb = 0)
 
 glafic.set_secondary('chi2_splane 0', verb = 0)
 glafic.set_secondary('chi2_checknimg 0', verb = 0)
@@ -11,11 +11,15 @@ glafic.set_secondary('hvary          0', verb = 0)
 glafic.set_secondary('ran_seed -122000', verb = 0)
 glafic.set_secondary('flag_hodensity 2', verb = 0)
 
-glafic.startup_setnum(1, 0, 1)
-glafic.set_lens(1, 'ein', 0.261343256161012, 4.79e+10, 20.80, 20.74, 0.2, -4.46, 50.0, 0.7)
-glafic.set_point(1, 1.0, 20.78, 20.78)
+glafic.startup_setnum(3, 0, 1)
+glafic.set_lens(1, 'ein', 0.261343256161012, 4.795440e+10, 2.080736e+01, 2.074175e+01, 2.012030e-01, -4.422571e+00, 4.999760e+01, 7.000000e-01)
+glafic.set_lens(2, 'sers', 0.261343256161012, 8e+10, 21.2807, 20.8498, 0.19174, 97.1557, 51.8810, 1.4424)
+glafic.set_lens(3, 'sers', 0.261343256161012, 1e+10, 21.2807, 20.8498, 0.58185, 80.9088, 5.97926, 0.5737)
+glafic.set_point(1, 1.0, 2.081290e+01, 2.077943e+01)
 
 glafic.setopt_lens(1, 0, 1, 1, 1, 1, 1, 1, 0)
+glafic.setopt_lens(2, 0, 1, 0, 0, 0, 0, 0, 0)
+glafic.setopt_lens(3, 0, 1, 0, 0, 0, 0, 0, 0)
 glafic.setopt_point(1, 0, 1, 1)
 
 # model_init needs to be done again whenever model parameters are changed
