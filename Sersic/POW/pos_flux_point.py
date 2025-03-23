4a1e6b1774059a3eda1cb65dab85f92062cb3642
rommuluslewis <rommuluslewis@gmail.com>
2025-03-23 20:12:42 +0800

    sersic pow pos+flux file update

--- Sersic/POW/pos_flux_point.py
+++ Sersic/POW/pos_flux_point.py
@@ -12,10 +12,10 @@ glafic.set_secondary('ran_seed -122000', verb = 0)
 
 glafic.startup_setnum(3, 0, 1)
 # pow index is 2.1 for POS constraint
-glafic.set_lens(1, 'pow', 0.261343256161012, 1.0, 2.080981e+01, 2.076027e+01, 1.164118e-01, -4.440819e+00, 4.908354e-01, 1.962414e+00) 
-glafic.set_lens(2, 'sers', 0.261343256161012, 8e+10, 21.2807, 20.8498, 0.19174, 97.1557, 51.8810, 1.4424)
-glafic.set_lens(3, 'sers', 0.261343256161012, 1e+10, 21.2807, 20.8498, 0.58185, 80.9088, 5.97926, 0.5737)
-glafic.set_point(1, 1.0, 2.081266e+01, 2.077968e+01)
+glafic.set_lens(1, 'pow', 0.261343256161012, 1.0, 2.080976e+01, 2.075982e+01, 1.237922e-01, -4.493337e+00, 4.895203e-01, 1.977869e+00) 
+glafic.set_lens(2, 'sers', 0.261343256161012, 7.950299e+10, 21.2807, 20.8498, 0.19174, 97.1557, 51.8810, 1.4424)
+glafic.set_lens(3, 'sers', 0.261343256161012, 9.913657e+09, 21.2807, 20.8498, 0.58185, 80.9088, 5.97926, 0.5737)
+glafic.set_point(1, 1.0, 2.081362e+01, 2.078027e+01)
 
 glafic.setopt_lens(1, 0, 0, 1, 1, 1, 1, 1, 1)
 glafic.setopt_lens(2, 0, 1, 0, 0, 0, 0, 0, 0)
@@ -30,6 +30,5 @@ glafic.optimize()
 glafic.findimg()
 glafic.writecrit(1.0)
 glafic.writelens(1.0)
-glafic.writemesh(1.0)
 
 glafic.quit()
