d0275046f4f2ba4e49d897b28e334ddf6a498ea4
rommuluslewis <rommuluslewis@gmail.com>
2025-03-25 01:14:51 +0800

    file update

--- kappa_glafic.py
+++ kappa_glafic.py
@@ -21,6 +21,6 @@ glafic.setopt_point(1, 0, 0, 0)
 glafic.model_init(verb = 0)
 
 glafic.readobs_point('/Users/ainsleylewis/Documents/Astronomy/IllustrisTNG Lens Modelling/obs_point/obs_point_(POS).dat')
-glafic.kapparad(1.0, 20.80, 20.80, 0, 2, 10, 1)
+glafic.kapparad(1.0, 20.8, 20.8, 0, 2, 10, 1)
 
 glafic.quit()
