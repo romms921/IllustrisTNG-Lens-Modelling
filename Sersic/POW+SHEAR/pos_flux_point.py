2ffd27d8b38774b092d0fdca846d556cb0eed37f
rommuluslewis <rommuluslewis@gmail.com>
2025-03-24 18:25:06 +0800

    sersic pow pos+flux+shear param update

--- Sersic/POW+SHEAR/pos_flux_point.py
+++ Sersic/POW+SHEAR/pos_flux_point.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import glafic
 
-glafic.init(0.3, 0.7, -1.0, 0.7, 'POW+SHEAR/POW_POS+FLUX_SHEAR', 20.0, 20.0, 21.56, 21.56, 0.01, 0.01, 1, verb = 0)
+glafic.init(0.3, 0.7, -1.0, 0.7, 'Sersic/POW+SHEAR/POW_POS+FLUX_SHEAR', 20.0, 20.0, 21.56, 21.56, 0.01, 0.01, 1, verb = 0)
 
 glafic.set_secondary('chi2_splane 0', verb = 0)
 glafic.set_secondary('chi2_checknimg 0', verb = 0)
@@ -10,13 +10,17 @@ glafic.set_secondary('chi2_usemag    1', verb = 0)
 glafic.set_secondary('hvary          0', verb = 0)
 glafic.set_secondary('ran_seed -122000', verb = 0)
 
-glafic.startup_setnum(2, 0, 1)
-glafic.set_lens(1, 'pow', 0.261343256161012, 1.0, 20.81, 20.76, 0.12, -4.0, 0.4, 2.1)
-glafic.set_lens(2, 'pert', 0.261343256161012, 1.0, 20.90, 20.64, 0.1, 140.0, 0.0, 0.3)
-glafic.set_point(1, 1.0, 20.78, 20.78)
+glafic.startup_setnum(4, 0, 1)
+glafic.set_lens(1, 'pow', 0.261343256161012, 1.0, 2.081000e+01, 2.076000e+01, 9.364637e-01, -1.359711e+00, 3.179559e-01, 2.930644e+00)
+glafic.set_lens(2, 'sers', 0.261343256161012, 8e+10, 21.2807, 20.8498, 0.19174, 97.1557, 51.8810, 1.4424)
+glafic.set_lens(3, 'sers', 0.261343256161012, 1e+10, 21.2807, 20.8498, 0.58185, 80.9088, 5.97926, 0.5737)
+glafic.set_lens(4, 'pert', 0.261343256161012, 1.0, 2.085513e+01, 2.048605e+01, 1.749327e-02, 1.718205e+02, 0.000000e+00, 0.0)
+glafic.set_point(1, 1.0, 2.084161e+01, 2.061858e+01)
 
 glafic.setopt_lens(1, 0, 0, 0, 0, 1, 1, 1, 1)
-glafic.setopt_lens(2, 0, 0, 1, 1, 1, 1, 0, 1)
+glafic.setopt_lens(2, 0, 1, 0, 0, 0, 0, 0, 0)
+glafic.setopt_lens(3, 0, 1, 0, 0, 0, 0, 0, 0)
+glafic.setopt_lens(4, 0, 0, 1, 1, 1, 1, 0, 0)
 glafic.setopt_point(1, 0, 1, 1)
 
 # model_init needs to be done again whenever model parameters are changed
