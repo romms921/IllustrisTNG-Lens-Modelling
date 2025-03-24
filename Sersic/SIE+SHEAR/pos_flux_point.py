dc6bafedf5fa4b51590012d39aa3a58197c79c11
rommuluslewis <rommuluslewis@gmail.com>
2025-03-24 12:31:41 +0800

    sersic sie+shear file added

---
+++ Sersic/SIE+SHEAR/pos_flux_point.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import glafic
+
+glafic.init(0.3, 0.7, -1.0, 0.7, 'SIE+SHEAR/SIE_POS+FLUX_SHEAR', 20.0, 20.0, 21.56, 21.56, 0.001, 0.001, 1, verb = 0)
+
+glafic.set_secondary('chi2_splane 0', verb = 0)
+glafic.set_secondary('chi2_checknimg 0', verb = 0)
+glafic.set_secondary('chi2_restart   -1', verb = 0)
+glafic.set_secondary('chi2_usemag    1', verb = 0)
+glafic.set_secondary('hvary          0', verb = 0)
+glafic.set_secondary('ran_seed -122000', verb = 0)
+
+glafic.startup_setnum(2, 0, 1)
+glafic.set_lens(1, 'sie', 0.261343256161012, 1.30e+02, 20.81, 20.76, 0.109, 23.18, 0.0, 0.0)
+glafic.set_lens(2, 'pert', 0.261343256161012, 1.0, 20.89, 20.64, 0.1, 140.0, 0.0, 0.3)
+glafic.set_point(1, 1.0, 20.78, 20.78)
+
+glafic.setopt_lens(1, 0, 1, 1, 1, 1, 1, 0, 0)
+glafic.setopt_lens(2, 0, 0, 1, 1, 1, 1, 0, 1)
+glafic.setopt_point(1, 0, 1, 1)
+
+# model_init needs to be done again whenever model parameters are changed
+glafic.model_init(verb = 0)
+
+glafic.readobs_point('/Users/ainsleylewis/Documents/Astronomy/IllustrisTNG Lens Modelling/obs_point/obs_point_(POS+FLUX).dat')
+glafic.optimize()
+glafic.findimg()
+glafic.writecrit(1.0)
+glafic.writelens(1.0)
+
+glafic.quit()
