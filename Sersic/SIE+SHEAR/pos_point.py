1b3f6b40d5b01b580772bedaa7fd453324e660c8
rommuluslewis <rommuluslewis@gmail.com>
2025-03-24 15:12:12 +0800

    sersic sie+pos+shear fixed run

--- Sersic/SIE+SHEAR/pos_point.py
+++ Sersic/SIE+SHEAR/pos_point.py
@@ -14,13 +14,13 @@ glafic.startup_setnum(4, 0, 1)
 glafic.set_lens(1, 'sie', 0.261343256161012, 1.346419e+02, 2.081302e+01, 2.076296e+01, 1.129813e-01, 2.122853e+01, 0.000000e+00, 0.000000e+00)
 glafic.set_lens(2, 'sers', 0.261343256161012, 8e+10, 21.2807, 20.8498, 0.19174, 97.1557, 51.8810, 1.4424)
 glafic.set_lens(3, 'sers', 0.261343256161012, 1e+10, 21.2807, 20.8498, 0.58185, 80.9088, 5.97926, 0.5737)
-glafic.set_lens(4, 'pert', 0.261343256161012, 1.0, 2.089792e+01, 2.063422e+01, 2.418515e-02, 1.403324e+02, 0.000000e+00, 2.730606e-01)
+glafic.set_lens(4, 'pert', 0.261343256161012, 1.0, 2.089792e+01, 2.063422e+01, 2.418515e-02, 1.403324e+02, 0.000000e+00, 0.0)
 glafic.set_point(1, 1.0, 2.084140e+01, 2.073864e+01)
 
 glafic.setopt_lens(1, 0, 1, 1, 1, 1, 1, 0, 0)
 glafic.setopt_lens(2, 0, 1, 0, 0, 0, 0, 0, 0)
 glafic.setopt_lens(3, 0, 1, 0, 0, 0, 0, 0, 0)
-glafic.setopt_lens(4, 0, 0, 1, 1, 1, 1, 0, 1)
+glafic.setopt_lens(4, 0, 0, 1, 1, 1, 1, 0, 0)
 glafic.setopt_point(1, 0, 1, 1)
 
 # model_init needs to be done again whenever model parameters are changed
