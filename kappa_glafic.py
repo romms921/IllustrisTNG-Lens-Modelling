ab43a7dba2741152817593a31d945779d45ee7ad
rommuluslewis <rommuluslewis@gmail.com>
2025-03-25 22:47:19 +0800

    general update

--- kappa_glafic.py
+++ kappa_glafic.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import glafic
 
-glafic.init(0.3, 0.7, -1.0, 0.7, 'SIE/SIE_POS', 20.0, 20.0, 21.56, 21.56, 0.01, 0.01, 1, verb = 0)
+glafic.init(0.3, 0.7, -1.0, 0.7, 'SIE/SIE_POS', 20.0, 20.0, 21.56, 21.56, 0.001, 0.001, 1, verb = 0)
 
 glafic.set_secondary('chi2_splane 0', verb = 0)
 glafic.set_secondary('chi2_checknimg 0', verb = 0)
@@ -14,13 +14,13 @@ glafic.startup_setnum(1, 0, 1)
 glafic.set_lens(1, 'sie', 0.2613, 1.580350e+02, 2.080960e+01, 2.075891e+01, 1.287589e-01, -4.437098e+00, 0.000000e+00, 0.000000e+00)
 glafic.set_point(1, 1.000, 2.081270e+01, 2.078004e+01)
 
-glafic.setopt_lens(1, 0, 0, 0, 0, 0, 0, 0, 0)
-glafic.setopt_point(1, 0, 0, 0)
+glafic.setopt_lens(1, 0, 1, 1, 1, 1, 1, 0, 0)
+glafic.setopt_point(1, 0, 1, 1)
 
 # model_init needs to be done again whenever model parameters are changed
 glafic.model_init(verb = 0)
 
 glafic.readobs_point('/Users/ainsleylewis/Documents/Astronomy/IllustrisTNG Lens Modelling/obs_point/obs_point_(POS).dat')
-glafic.kapparad(1.0, 20.8, 20.8, 0, 2, 10, 1)
+glafic.kapparad(1, 20.8, 20.8, 0, 2, 10, 1)
 
 glafic.quit()
