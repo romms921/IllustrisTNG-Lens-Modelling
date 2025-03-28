25deb4558f1173418ca8a21d456011da49749f50
rommuluslewis <rommuluslewis@gmail.com>
2025-03-28 13:34:06 +0800

    updated mass profile plot

--- kappa_glafic.py
+++ kappa_glafic.py
@@ -21,7 +21,7 @@ glafic.setopt_point(1, 0, 0, 0)
 glafic.model_init(verb = 0)
 
 glafic.readobs_point('/Users/ainsleylewis/Documents/Astronomy/IllustrisTNG Lens Modelling/obs_point/obs_point_(POS).dat')
-glafic.kapparad(1.0000, 2.081270e+01, 2.078004e+01, 0.03, 1.0, 1000, 1)
-glafic.kappacum(1.0000, 2.081270e+01, 2.078004e+01, 0.03, 1.0, 1000, 1)
+glafic.kapparad(1.0000, 2.081270e+01, 2.078004e+01, 0.03, 0.8, 1000, 1)
+glafic.kappacum(1.0000, 2.081270e+01, 2.078004e+01, 0.03, 0.8, 1000, 1)
 
 glafic.quit()
