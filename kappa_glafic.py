0b5361e9dbd6e934fd15d233d72bbcf6548226bc
rommuluslewis <rommuluslewis@gmail.com>
2025-03-28 16:48:07 +0800

    rerun mass profiles with updated plot

--- kappa_glafic.py
+++ kappa_glafic.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import glafic
 
-glafic.init(0.3, 0.7, -1.0, 0.7, 'NFW/NFW_POS', 20.0, 20.0, 21.56, 21.56, 0.01, 0.01, 1, verb = 0)
+glafic.init(0.3, 0.7, -1.0, 0.7, 'EIN/EIN_POS+FLUX', 20.0, 20.0, 21.56, 21.56, 0.01, 0.01, 1, verb = 0)
 
 glafic.set_secondary('chi2_splane 0', verb = 0)
 glafic.set_secondary('chi2_checknimg 0', verb = 0)
@@ -11,8 +11,8 @@ glafic.set_secondary('hvary          0', verb = 0)
 glafic.set_secondary('ran_seed -122000', verb = 0)
 
 glafic.startup_setnum(1, 0, 1)
-glafic.set_lens(1, 'anfw', 0.2613, 2.085312e+11, 2.080884e+01, 2.075341e+01, 1.500761e-01, -4.431327e+00, 8.699047e+01, 0.000000e+00)
-glafic.set_point(1, 1.000, 1.0000, 2.081271e+01)
+glafic.set_lens(1, 'ein', 0.2613, 6.526618e+10, 2.080840e+01, 2.075116e+01, 1.479562e-01, -4.444363e+00, 4.999970e+01, 5.000000e-01)
+glafic.set_point(1, 1.000, 1.0000, 2.081241e+01)
 
 glafic.setopt_lens(1, 0, 0, 0, 0, 0, 0, 0, 0)
 glafic.setopt_point(1, 0, 0, 0)
@@ -21,7 +21,7 @@ glafic.setopt_point(1, 0, 0, 0)
 glafic.model_init(verb = 0)
 
 glafic.readobs_point('/Users/ainsleylewis/Documents/Astronomy/IllustrisTNG Lens Modelling/obs_point/obs_point_(POS).dat')
-glafic.kapparad(1.0000, 2.081271e+01, 2.077975e+01, 0.03, 0.8, 1000, 1)
-glafic.kappacum(1.0000, 2.081271e+01, 2.077975e+01, 0.03, 0.8, 1000, 1)
+glafic.kapparad(1.0000, 2.081241e+01, 2.077851e+01, 0.03, 0.8, 1000, 1)
+glafic.kappacum(1.0000, 2.081241e+01, 2.077851e+01, 0.03, 0.8, 1000, 1)
 
 glafic.quit()
