e3961990fcba7d62efa131eda0e0243acdd84f27
rommuluslewis <rommuluslewis@gmail.com>
2025-04-01 03:15:09 +0800

    added kappa shear py file

---
+++ kappa_shear_glafic.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+import glafic
+
+glafic.init(0.3, 0.7, -1.0, 0.7, 'Kappa/SIE+SHEAR/SIE_POS_SHEAR', 20.0, 20.0, 21.56, 21.56, 0.001, 0.001, 1, verb = 0)
+
+glafic.set_secondary('chi2_splane 0', verb = 0)
+glafic.set_secondary('chi2_checknimg 0', verb = 0)
+glafic.set_secondary('chi2_restart   -1', verb = 0)
+glafic.set_secondary('chi2_usemag    1', verb = 0)
+glafic.set_secondary('hvary          0', verb = 0)
+glafic.set_secondary('ran_seed -122000', verb = 0)
+
+glafic.startup_setnum(2, 0, 1)
+glafic.set_lens(1, 'pert', 0.2613, 1.000000e+00, 2.089792e+01, 2.063422e+01, 2.418515e-02, 1.403324e+02, 0.000000e+00, 2.730606e-01)
+glafic.set_lens(2, 'pert', 0.2613, 1.000000e+00, 2.089792e+01, 2.063422e+01, 2.418515e-02, 1.403324e+02, 0.000000e+00, 2.730606e-01)
+glafic.set_point(1, 1.000, 2.084140e+01, 2.073864e+01)
+
+glafic.setopt_lens(1, 0, 0, 0, 0, 0, 0, 0, 0)
+glafic.setopt_lens(2, 0, 0, 0, 0, 0, 0, 0, 0)
+glafic.setopt_point(1, 0, 0, 0)
+
+# model_init needs to be done again whenever model parameters are changed
+glafic.model_init(verb = 0)
+
+glafic.readobs_point('/Users/ainsleylewis/Documents/Astronomy/IllustrisTNG Lens Modelling/obs_point/obs_point_(POS).dat')
+glafic.writelens(1.0)
+
+glafic.quit()
