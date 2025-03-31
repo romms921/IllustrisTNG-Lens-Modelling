d70c697b1f11285f25e4889c502c030c20b8ca9b
rommuluslewis <rommuluslewis@gmail.com>
2025-04-01 03:23:58 +0800

    shear mass profile running

--- kappa_shear_glafic.py
+++ kappa_shear_glafic.py
@@ -11,9 +11,9 @@ glafic.set_secondary('hvary          0', verb = 0)
 glafic.set_secondary('ran_seed -122000', verb = 0)
 
 glafic.startup_setnum(2, 0, 1)
-glafic.set_lens(1, 'sie', 0.2613, 1.00, 2.081, 2.06, 2.418515e-02, 1.403324e+02, 0.000000e+00, 2.730606e-01)
-glafic.set_lens(2, 'pert', 0.2613, 1.000, 2.089, 2.06342, 2.418515e-02, 1.403324e+02, 0.000000e+00, 2.730606e-01)
-glafic.set_point(1, 1.000, 2.084101, 2.073801)
+glafic.set_lens(1, 'sie', 0.2613, 1.346419e+02, 2.081302e+01, 2.076296e+01, 1.129813e-01, 2.122853e+01, 0.000000e+00, 0.000000e+00)
+glafic.set_lens(2, 'pert', 0.2613, 1.000000e+00, 2.089792e+01, 2.063422e+01, 2.418515e-02, 1.403324e+02, 0.000000e+00, 2.730606e-01)
+glafic.set_point(1, 1.000, 2.084140e+01, 2.073864e+01)
 
 glafic.setopt_lens(1, 0, 0, 0, 0, 0, 0, 0, 0)
 glafic.setopt_lens(2, 0, 0, 0, 0, 0, 0, 0, 0)
