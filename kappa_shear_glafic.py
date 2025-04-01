565e3ea322c0675b095dff08cd0fd3aa048446d0
rommuluslewis <rommuluslewis@gmail.com>
2025-04-01 12:53:48 +0800

    nfw pos + flux + shear

--- kappa_shear_glafic.py
+++ kappa_shear_glafic.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import glafic
 
-glafic.init(0.3, 0.7, -1.0, 0.7, 'Kappa/NFW+SHEAR/NFW_POS_SHEAR', 20.0, 20.0, 21.56, 21.56, 0.001, 0.001, 1, verb = 0)
+glafic.init(0.3, 0.7, -1.0, 0.7, 'Kappa/NFW+SHEAR/NFW_POS+FLUX_SHEAR', 20.0, 20.0, 21.56, 21.56, 0.001, 0.001, 1, verb = 0)
 
 glafic.set_secondary('chi2_splane 0', verb = 0)
 glafic.set_secondary('chi2_checknimg 0', verb = 0)
@@ -11,9 +11,9 @@ glafic.set_secondary('hvary          0', verb = 0)
 glafic.set_secondary('ran_seed -122000', verb = 0)
 
 glafic.startup_setnum(2, 0, 1)
-glafic.set_lens(1, 'anfw', 0.2613, 1.841836e+11, 2.081199e+01, 2.075357e+01, 1.579358e-01, 8.657437e+00, 8.701885e+01, 0.000000e+00)
-glafic.set_lens(2, 'pert', 0.2613, 1.000000e+00, 2.080820e+01, 2.064165e+01, 2.469017e-02, 1.333287e+02, 0.000000e+00, 7.921322e-02)
-glafic.set_point(1, 1.000, 2.081794e+01, 2.076874e+01)
+glafic.set_lens(1, 'anfw', 0.2613, 2.268691e+11, 2.081333e+01, 2.076800e+01, 6.882355e-02, 3.531638e+01, 7.998061e+01, 0.000000e+00)
+glafic.set_lens(2, 'pert', 0.2613, 1.000000e+00, 2.074037e+01, 2.084955e+01, 3.262113e-02, 1.503822e+02, 0.000000e+00, 1.000000e-04)
+glafic.set_point(1, 1.000, 2.081152e+01, 2.078058e+01)
 
 glafic.setopt_lens(1, 0, 0, 0, 0, 0, 0, 0, 0)
 glafic.setopt_lens(2, 0, 0, 0, 0, 0, 0, 0, 0)
