996d5063a657c8ca90797ea13c090198da3f59b1
rommuluslewis <rommuluslewis@gmail.com>
2025-04-02 15:45:29 +0800

    cored sie shear comparison run

--- kappa_shear_glafic.py
+++ kappa_shear_glafic.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import glafic
 
-glafic.init(0.3, 0.7, -1.0, 0.7, 'Kappa/POW+SHEAR/POW_POS+FLUX_SHEAR', 20.0, 20.0, 21.56, 21.56, 0.001, 0.001, 1, verb = 0)
+glafic.init(0.3, 0.7, -1.0, 0.7, 'Kappa/C_SIE+SHEAR/C_SIE_POS_SHEAR', 20.0, 20.0, 21.56, 21.56, 0.001, 0.001, 1, verb = 0)
 
 glafic.set_secondary('chi2_splane 0', verb = 0)
 glafic.set_secondary('chi2_checknimg 0', verb = 0)
@@ -11,9 +11,9 @@ glafic.set_secondary('hvary          0', verb = 0)
 glafic.set_secondary('ran_seed -122000', verb = 0)
 
 glafic.startup_setnum(2, 0, 1)
-glafic.set_lens(1, 'pow', 0.2613, 1.000000e+00, 2.081000e+01, 2.076000e+01, 1.483179e-01, 1.013373e+01, 4.290774e-01, 2.200000e+00)
-glafic.set_lens(2, 'pert', 0.2613, 1.000000e+00, 2.090772e+01, 2.060688e+01, 2.235114e-02, 1.454115e+02, 0.000000e+00, 1.488050e-01)
-glafic.set_point(1, 1.000, 2.083151e+01, 2.075728e+01)
+glafic.set_lens(1, 'sie', 0.2613, 1.640646e+02, 2.081341e+01, 2.076613e+01, 8.474830e-02, 3.087715e+01, 3.817197e-02, 0.000000e+00)
+glafic.set_lens(2, 'pert', 0.2613, 1.000000e+00, 2.088229e+01, 2.068053e+01, 3.406566e-02, 1.449960e+02, 0.000000e+00, 1.000000e-03)
+glafic.set_point(1, 1.000, 2.081883e+01, 2.077747e+01)
 
 glafic.setopt_lens(1, 0, 0, 0, 0, 0, 0, 0, 0)
 glafic.setopt_lens(2, 0, 0, 0, 0, 0, 0, 0, 0)
