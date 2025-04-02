4124705f9a3648eff24c5f9ad538bf1189b349a1
rommuluslewis <rommuluslewis@gmail.com>
2025-04-02 15:11:55 +0800

    pow pos+flux+shear run

--- POW+SHEAR/pos_flux_point.py
+++ POW+SHEAR/pos_flux_point.py
@@ -3,7 +3,7 @@ import glafic
 
 glafic.init(0.3, 0.7, -1.0, 0.7, 'POW+SHEAR/POW_POS+FLUX_SHEAR', 20.0, 20.0, 21.56, 21.56, 0.01, 0.01, 1, verb = 0)
 
-glafic.set_secondary('chi2_splane 0', verb = 0)
+glafic.set_secondary('chi2_splane 1', verb = 0)
 glafic.set_secondary('chi2_checknimg 0', verb = 0)
 glafic.set_secondary('chi2_restart   -1', verb = 0)
 glafic.set_secondary('chi2_usemag    1', verb = 0)
@@ -11,11 +11,11 @@ glafic.set_secondary('hvary          0', verb = 0)
 glafic.set_secondary('ran_seed -122000', verb = 0)
 
 glafic.startup_setnum(2, 0, 1)
-glafic.set_lens(1, 'pow', 0.261343256161012, 1.0, 20.81, 20.76, 0.12, -4.0, 0.4, 2.1)
+glafic.set_lens(1, 'pow', 0.261343256161012, 1.0, 20.81, 20.76, 0.12, -4.0, 0.4, 2.2)
 glafic.set_lens(2, 'pert', 0.261343256161012, 1.0, 20.90, 20.64, 0.1, 140.0, 0.0, 0.3)
 glafic.set_point(1, 1.0, 20.78, 20.78)
 
-glafic.setopt_lens(1, 0, 0, 0, 0, 1, 1, 1, 1)
+glafic.setopt_lens(1, 0, 0, 0, 0, 1, 1, 1, 0)
 glafic.setopt_lens(2, 0, 0, 1, 1, 1, 1, 0, 1)
 glafic.setopt_point(1, 0, 1, 1)
 
--- kappa_shear_glafic.py
+++ kappa_shear_glafic.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import glafic
 
-glafic.init(0.3, 0.7, -1.0, 0.7, 'Kappa/NFW+SHEAR/NFW_POS+FLUX_SHEAR', 20.0, 20.0, 21.56, 21.56, 0.001, 0.001, 1, verb = 0)
+glafic.init(0.3, 0.7, -1.0, 0.7, 'Kappa/POW+SHEAR/POW_POS+FLUX_SHEAR', 20.0, 20.0, 21.56, 21.56, 0.001, 0.001, 1, verb = 0)
 
 glafic.set_secondary('chi2_splane 0', verb = 0)
 glafic.set_secondary('chi2_checknimg 0', verb = 0)
@@ -11,9 +11,9 @@ glafic.set_secondary('hvary          0', verb = 0)
 glafic.set_secondary('ran_seed -122000', verb = 0)
 
 glafic.startup_setnum(2, 0, 1)
-glafic.set_lens(1, 'anfw', 0.2613, 2.268691e+11, 2.081333e+01, 2.076800e+01, 6.882355e-02, 3.531638e+01, 7.998061e+01, 0.000000e+00)
-glafic.set_lens(2, 'pert', 0.2613, 1.000000e+00, 2.074037e+01, 2.084955e+01, 3.262113e-02, 1.503822e+02, 0.000000e+00, 1.000000e-04)
-glafic.set_point(1, 1.000, 2.081152e+01, 2.078058e+01)
+glafic.set_lens(1, 'pow', 0.2613, 1.000000e+00, 2.081000e+01, 2.076000e+01, 9.364637e-01, -1.359711e+00, 3.179559e-01, 2.930644e+00)
+glafic.set_lens(2, 'pert', 0.2613, 1.000000e+00, 2.085513e+01, 2.048605e+01, 1.749327e-02, 1.718205e+02, 0.000000e+00, 5.887661e-01)
+glafic.set_point(1, 1.000, 2.084161e+01, 2.061858e+01)
 
 glafic.setopt_lens(1, 0, 0, 0, 0, 0, 0, 0, 0)
 glafic.setopt_lens(2, 0, 0, 0, 0, 0, 0, 0, 0)
