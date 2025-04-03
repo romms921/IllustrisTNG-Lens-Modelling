e6cdfb44ae90d3cd0b89c23ddf2b826476dedc6c
rommuluslewis <rommuluslewis@gmail.com>
2025-04-03 14:24:32 +0800

    general update

--- kappa_glafic.py
+++ kappa_glafic.py
@@ -11,7 +11,7 @@ glafic.set_secondary('hvary          0', verb = 0)
 glafic.set_secondary('ran_seed -122000', verb = 0)
 
 glafic.startup_setnum(1, 0, 1)
-glafic.set_lens(1, 'pert', 0.2613, 1.000000e+00, 2.089792e+01, 2.063422e+01, 2.418515e-02, 1.403324e+02, 0.000000e+00, 2.730606e-01)
+glafic.set_lens(1, 'sie', 0.2613, 1.346419e+02, 2.081302e+01, 2.076296e+01, 1.129813e-01, 2.122853e+01, 0.000000e+00, 0.000000e+00)
 glafic.set_point(1, 1.000, 2.084140e+01, 2.073864e+01)
 
 glafic.setopt_lens(1, 0, 0, 0, 0, 0, 0, 0, 0)
