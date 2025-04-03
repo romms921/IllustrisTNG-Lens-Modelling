d577a24d12ee6e91dd5b0e24bc8a7c45764d88fd
rommuluslewis <rommuluslewis@gmail.com>
2025-04-03 11:53:23 +0800

    fixed comparison plots with common cbar

--- kappa_glafic.py
+++ kappa_glafic.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import glafic
 
-glafic.init(0.3, 0.7, -1.0, 0.7, 'kappa/EIN/EIN_POS', 20.0, 20.0, 21.56, 21.56, 0.001, 0.001, 1, verb = 0)
+glafic.init(0.3, 0.7, -1.0, 0.7, 'kappa/SIE+SHEAR/SIE_POS_SHEAR', 20.0, 20.0, 21.56, 21.56, 0.001, 0.001, 1, verb = 0)
 
 glafic.set_secondary('chi2_splane 0', verb = 0)
 glafic.set_secondary('chi2_checknimg 0', verb = 0)
@@ -11,8 +11,8 @@ glafic.set_secondary('hvary          0', verb = 0)
 glafic.set_secondary('ran_seed -122000', verb = 0)
 
 glafic.startup_setnum(1, 0, 1)
-glafic.set_lens(1, 'ein', 0.2613, 4.795440e+10, 2.080736e+01, 2.074175e+01, 2.012030e-01, -4.422571e+00, 4.999760e+01, 7.000000e-01)
-glafic.set_point(1, 1.000, 2.081290e+01, 2.077943e+01)
+glafic.set_lens(1, 'pert', 0.2613, 1.000000e+00, 2.089792e+01, 2.063422e+01, 2.418515e-02, 1.403324e+02, 0.000000e+00, 2.730606e-01)
+glafic.set_point(1, 1.000, 2.084140e+01, 2.073864e+01)
 
 glafic.setopt_lens(1, 0, 0, 0, 0, 0, 0, 0, 0)
 glafic.setopt_point(1, 0, 0, 0)
