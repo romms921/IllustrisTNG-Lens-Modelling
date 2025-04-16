a98cede686243136f5b3e1b89c66c97ea5a41d4b
rommuluslewis <rommuluslewis@gmail.com>
2025-04-16 16:18:56 +0800

    added nfw+mpole

---
+++ NFW+MPOLE/pos_point.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import glafic
+
+glafic.init(0.3, 0.7, -1.0, 0.7, 'NFW+MPOLE/NFW_POS_MPOLE', 20.0, 20.0, 21.56, 21.56, 0.01, 0.01, 1, verb = 0)
+
+glafic.set_secondary('chi2_splane 0', verb = 0)
+glafic.set_secondary('chi2_checknimg 0', verb = 0)
+glafic.set_secondary('chi2_restart   -1', verb = 0)
+glafic.set_secondary('chi2_usemag    1', verb = 0)
+glafic.set_secondary('hvary          0', verb = 0)
+glafic.set_secondary('ran_seed -122000', verb = 0)
+
+glafic.startup_setnum(2, 0, 1)
+glafic.set_lens(1, 'anfw', 0.261343256161012, 2.0e+11, 20.80, 20.75, 0.14, -4.4, 87.0, 0.0)
+glafic.set_lens(2, 'mpole', 0.261343256161012, 1.0, 20.80, 20.75, 0.02, -90.0, 4.5, 0.0)
+glafic.set_point(1, 1.0, 20.78, 20.78)
+
+glafic.setopt_lens(1, 0, 1, 1, 1, 1, 1, 1, 0)
+glafic.setopt_lens(2, 0, 0, 1, 1, 1, 1, 1, 0)
+glafic.setopt_point(1, 0, 1, 1)
+
+# model_init needs to be done again whenever model parameters are changed
+glafic.model_init(verb = 0)
+
+glafic.readobs_point('/Users/ainsleylewis/Documents/Astronomy/IllustrisTNG Lens Modelling/obs_point/obs_point_(POS).dat')
+glafic.optimize()
+glafic.findimg()
+glafic.writecrit(1.0)
+glafic.writelens(1.0)
+
+glafic.quit()
