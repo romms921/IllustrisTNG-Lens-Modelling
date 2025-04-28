cc089b65166272a1d4e3ef5a1b4f0bad88b31d66
rommuluslewis <rommuluslewis@gmail.com>
2025-04-28 18:51:58 +0800

    added plots to main.ipynb

--- Sersic/C_SIE/pos_point.py
+++ Sersic/C_SIE/pos_point.py
@@ -1,20 +1,24 @@
 #!/usr/bin/env python
 import glafic
 
-glafic.init(0.3, 0.7, -1.0, 0.7, 'C_SIE/C_SIE_POS', 20.0, 20.0, 21.56, 21.56, 0.01, 0.01, 1, verb = 0)
+glafic.init(0.3, 0.7, -1.0, 0.7, 'Sersic/C_SIE/C_SIE_POS', 20.0, 20.0, 21.56, 21.56, 0.01, 0.01, 1, verb = 0)
 
-glafic.set_secondary('chi2_splane 0', verb = 0)
+glafic.set_secondary('chi2_splane 1', verb = 0)
 glafic.set_secondary('chi2_checknimg 0', verb = 0)
 glafic.set_secondary('chi2_restart   -1', verb = 0)
 glafic.set_secondary('chi2_usemag    1', verb = 0)
 glafic.set_secondary('hvary          0', verb = 0)
 glafic.set_secondary('ran_seed -122000', verb = 0)
 
-glafic.startup_setnum(1, 0, 1)
-glafic.set_lens(1, 'sie', 0.261343256161012, 1.63e+02,  20.80, 20.75, 0.11, -4.4, 0.05, 0.0)
+glafic.startup_setnum(3, 0, 1)
+glafic.set_lens(1, 'sie', 0.261343256161012, 1.63e+02,  20.80, 20.75, 0.11, -4.4, 0.01, 0.0)
+glafic.set_lens(2, 'sers', 0.261343256161012, 8e+10, 21.2807, 20.8498, 0.19174, 97.1557, 51.8810, 1.4424)
+glafic.set_lens(3, 'sers', 0.261343256161012, 1e+10, 21.2807, 20.8498, 0.58185, 80.9088, 5.97926, 0.5737)
 glafic.set_point(1, 1.0, 20.78, 20.78)
 
 glafic.setopt_lens(1, 0, 1, 1, 1, 1, 1, 1, 0)
+glafic.setopt_lens(2, 0, 1, 0, 0, 0, 0, 0, 0)
+glafic.setopt_lens(3, 0, 1, 0, 0, 0, 0, 0, 0)
 glafic.setopt_point(1, 0, 1, 1)
 
 # model_init needs to be done again whenever model parameters are changed
