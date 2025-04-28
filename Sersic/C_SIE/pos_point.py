7ff22b2dec89332e49ce12e675a6b8c1152f3304
rommuluslewis <rommuluslewis@gmail.com>
2025-04-28 16:42:20 +0800

    added csie to sersic

---
+++ Sersic/C_SIE/pos_point.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+import glafic
+
+glafic.init(0.3, 0.7, -1.0, 0.7, 'C_SIE/C_SIE_POS', 20.0, 20.0, 21.56, 21.56, 0.01, 0.01, 1, verb = 0)
+
+glafic.set_secondary('chi2_splane 0', verb = 0)
+glafic.set_secondary('chi2_checknimg 0', verb = 0)
+glafic.set_secondary('chi2_restart   -1', verb = 0)
+glafic.set_secondary('chi2_usemag    1', verb = 0)
+glafic.set_secondary('hvary          0', verb = 0)
+glafic.set_secondary('ran_seed -122000', verb = 0)
+
+glafic.startup_setnum(1, 0, 1)
+glafic.set_lens(1, 'sie', 0.261343256161012, 1.63e+02,  20.80, 20.75, 0.11, -4.4, 0.05, 0.0)
+glafic.set_point(1, 1.0, 20.78, 20.78)
+
+glafic.setopt_lens(1, 0, 1, 1, 1, 1, 1, 1, 0)
+glafic.setopt_point(1, 0, 1, 1)
+
+# model_init needs to be done again whenever model parameters are changed
+glafic.model_init(verb = 0)
+
+glafic.readobs_point('/Users/ainsleylewis/Documents/Astronomy/IllustrisTNG Lens Modelling/obs_point/obs_point_(POS).dat')
+glafic.optimize()
+glafic.findimg()
+glafic.writecrit(1.0)
+glafic.writelens(1.0)
+
+glafic.quit()
