9aaeb62377c509dbc418318126dadd920cd61f6c
rommuluslewis <rommuluslewis@gmail.com>
2025-06-13 16:44:23 +0530

    mass vectors run + diff shear values tested

--- kappa_glafic.py
+++ kappa_glafic.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import glafic
 
-glafic.init(0.3, 0.7, -1.0, 0.7, 'kappa/SIE+SHEAR/SIE_POS_SHEAR', 20.0, 20.0, 21.56, 21.56, 0.001, 0.001, 1, verb = 0)
+glafic.init(0.3, 0.7, -1.0, 0.7, 'Kappa/SIE/SIE_POS', 20.0, 20.0, 21.56, 21.56, 0.001, 0.001, 1, verb = 0)
 
 glafic.set_secondary('chi2_splane 0', verb = 0)
 glafic.set_secondary('chi2_checknimg 0', verb = 0)
@@ -11,8 +11,8 @@ glafic.set_secondary('hvary          0', verb = 0)
 glafic.set_secondary('ran_seed -122000', verb = 0)
 
 glafic.startup_setnum(1, 0, 1)
-glafic.set_lens(1, 'sie', 0.2613, 1.346419e+02, 2.081302e+01, 2.076296e+01, 1.129813e-01, 2.122853e+01, 0.000000e+00, 0.000000e+00)
-glafic.set_point(1, 1.000, 2.084140e+01, 2.073864e+01)
+glafic.set_lens(1, 'sie', 0.2613, 1.581390e+02, 2.080936e+01, 2.075881e+01, 1.215561e-01, -4.463959e+00, 0.000000e+00, 0.000000e+00)
+glafic.set_point(1, 1.000, 2.081228e+01, 2.077870e+01)
 
 glafic.setopt_lens(1, 0, 0, 0, 0, 0, 0, 0, 0)
 glafic.setopt_point(1, 0, 0, 0)
