ea0b61e703dcf279cf8d88cf8f990379fde22728
rommuluslewis <rommuluslewis@gmail.com>
2025-06-16 17:25:33 +0530

    added observed and predicted positions

--- Test/kappa.py
+++ Test/kappa.py
@@ -11,7 +11,7 @@ glafic.set_secondary('hvary          0', verb = 0)
 glafic.set_secondary('ran_seed -122000', verb = 0)
 
 glafic.startup_setnum(1, 0, 1)
-glafic.set_lens(1, "pert", 0.261343256161012, 1.0, 20.78, 20.78, 4.0, 50.0, 0.0, 3.0)
+glafic.set_lens(1, "pert", 0.261343256161012, 1.0, 20.78, 20.78, 0.0, 0.0, 0.0, 0.0)
 glafic.set_point(1, 1.000, 20.78, 20.78)
 
 glafic.setopt_lens(1, 0, 0, 0, 0, 0, 0, 0, 0)
