e465251a1cfa7e13dcb639a0578316f667d5ddc2
rommuluslewis <rommuluslewis@gmail.com>
2025-06-24 20:19:48 +0800

    changed write_lens.py

--- Test/kappa.py
+++ Test/kappa.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import glafic
 
-glafic.init(0.3, 0.7, -1.0, 0.7, "Test/SIE+SHEAR/pert", 20.0, 20.0, 21.56, 21.56, 0.01, 0.01, 1, verb = 0)
+glafic.init(0.3, 0.7, -1.0, 0.7, Sersic/SIE/SIE_POS, -0.78, -0.78, 0.78, 0.78, 0.012, 0.012, 1, verb = 0)
 
 glafic.set_secondary('chi2_splane 1', verb = 0)
 glafic.set_secondary('chi2_checknimg 0', verb = 0)
@@ -10,17 +10,24 @@ glafic.set_secondary('chi2_usemag    1', verb = 0)
 glafic.set_secondary('hvary          0', verb = 0)
 glafic.set_secondary('ran_seed -122000', verb = 0)
 
-glafic.startup_setnum(1, 0, 1)
-glafic.set_lens(1, "pert", 0.261343256161012, 1.0, 20.81583, 20.74901, 0.06537414, 152.417, 0.0, -0.4100367)
-glafic.set_point(1, 1.000, 20.81666, 20.79329)
+glafic.startup_setnum(3)
+glafic.startup_setnum(3)
+glafic.set_lens(1, "sie", 0.261343256161012, 157.9333, 20.80932, 20.75843, 0.1261772, -4.523281, 0.0, 0.0)
+glafic.set_lens(1, "sie", 0.261343256161012, 157.9333, 20.80932, 20.75843, 0.1261772, -4.523281, 0.0, 0.0)
+glafic.set_lens(2, sers, 0.261343256161012, 79706850000.0, 20.80932, 20.75843, 0.19174, 97.1557, 51.881, 1.4424)
+glafic.set_lens(2, sers, 0.261343256161012, 79706850000.0, 20.80932, 20.75843, 0.19174, 97.1557, 51.881, 1.4424)
+glafic.set_lens(3, sers, 0.261343256161012, 9487091000.0, 20.80932, 20.75843, 0.58185, 80.9088, 5.97926, 0.5737)
+# glafic.set_lens(4)
+glafic.set_point(1, 1.000, 20.8123, 20.77867)
 
-glafic.setopt_lens(1, 0, 0, 0, 0, 0, 0, 0, 0)
-glafic.setopt_point(1, 0, 0, 0)
+glafic.setopt_lens(1, 0, 0, 1, 1, 1, 1, 1, 0)
+glafic.setopt_lens(2, 0, 1, 1, 1, 0, 0, 0, 0)
+glafic.setopt_lens(3, 0, 1, 1, 1, 0, 0, 0, 0)
+glafic.setopt_point(1, 0, 1, 1)
 
 # model_init needs to be done again whenever model parameters are changed
 glafic.model_init(verb = 0)
 
-glafic.readobs_point('/Users/ainsleylewis/Documents/Astronomy/IllustrisTNG Lens Modelling/obs_point/obs_point_(POS).dat')
 glafic.writelens(1.0)
 
 glafic.quit()
