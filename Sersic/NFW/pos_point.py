601cafc3465ea53e60cf589f43d323ca5abcdbac
rommuluslewis <rommuluslewis@gmail.com>
2025-06-24 18:40:16 +0800

    added prior file and re run models with priors

--- Sersic/NFW/pos_point.py
+++ Sersic/NFW/pos_point.py
@@ -3,7 +3,7 @@ import glafic
 
 glafic.init(0.3, 0.7, -1.0, 0.7, 'Sersic/NFW/NFW_POS', 20.0, 20.0, 21.56, 21.56, 0.01, 0.01, 1, verb = 0)
 
-glafic.set_secondary('chi2_splane 0', verb = 0)
+glafic.set_secondary('chi2_splane 1', verb = 0)
 glafic.set_secondary('chi2_checknimg 0', verb = 0)
 glafic.set_secondary('chi2_restart   -1', verb = 0)
 glafic.set_secondary('chi2_usemag    1', verb = 0)
@@ -13,8 +13,8 @@ glafic.set_secondary('flag_hodensity 2', verb = 0)
 
 glafic.startup_setnum(3, 0, 1)
 glafic.set_lens(1, 'anfw', 0.261343256161012, 2.085312e+11, 2.080884e+01, 2.075341e+01, 1.500761e-01, -4.431327e+00,  8.699047e+01, 0.000000e+00)
-glafic.set_lens(2, 'sers', 0.261343256161012, 8e+10, 21.2807, 20.8498, 0.19174, 97.1557, 51.8810, 1.4424)
-glafic.set_lens(3, 'sers', 0.261343256161012, 1e+10, 21.2807, 20.8498, 0.58185, 80.9088, 5.97926, 0.5737)
+glafic.set_lens(2, 'sers', 0.261343256161012, 8e+10, 20.8512217, 20.8340052, 0.19174, 97.1557, 51.8810, 1.4424)
+glafic.set_lens(3, 'sers', 0.261343256161012, 1e+10, 20.8512217, 20.8340052, 0.58185, 80.9088, 5.97926, 0.5737)
 glafic.set_point(1, 1.0, 2.081271e+01, 2.077975e+01)
 
 glafic.setopt_lens(1, 0, 1, 1, 1, 1, 1, 1, 0)
@@ -26,6 +26,7 @@ glafic.setopt_point(1, 0, 1, 1)
 glafic.model_init(verb = 0)
 
 glafic.readobs_point('/Users/ainsleylewis/Documents/Astronomy/IllustrisTNG Lens Modelling/obs_point/obs_point_(POS).dat')
+glafic.parprior("/Users/ainsleylewis/Documents/Astronomy/IllustrisTNG Lens Modelling/priorfile.dat")
 glafic.optimize()
 glafic.findimg()
 glafic.writecrit(1.0)
--- Sersic/POW+SHEAR/pos_point.py
+++ Sersic/POW+SHEAR/pos_point.py
@@ -3,7 +3,7 @@ import glafic
 
 glafic.init(0.3, 0.7, -1.0, 0.7, 'Sersic/POW+SHEAR/POW_POS_SHEAR', 20.0, 20.0, 21.56, 21.56, 0.01, 0.01, 1, verb = 0)
 
-glafic.set_secondary('chi2_splane 0', verb = 0)
+glafic.set_secondary('chi2_splane 1', verb = 0)
 glafic.set_secondary('chi2_checknimg 0', verb = 0)
 glafic.set_secondary('chi2_restart   -1', verb = 0)
 glafic.set_secondary('chi2_usemag    1', verb = 0)
@@ -12,14 +12,14 @@ glafic.set_secondary('ran_seed -122000', verb = 0)
 
 glafic.startup_setnum(4, 0, 1)
 glafic.set_lens(1, 'pow', 0.261343256161012, 1.0, 2.081000e+01, 2.076000e+01, 1.618006e-01, 1.213509e+01, 3.418434e-01, 2.240000e+00)
-glafic.set_lens(2, 'sers', 0.261343256161012, 8e+10, 21.2807, 20.8498, 0.19174, 97.1557, 51.8810, 1.4424)
-glafic.set_lens(3, 'sers', 0.261343256161012, 1e+10, 21.2807, 20.8498, 0.58185, 80.9088, 5.97926, 0.5737)
+glafic.set_lens(2, 'sers', 0.261343256161012, 8e+10, 20.8512217, 20.8340052, 0.19174, 97.1557, 51.8810, 1.4424)
+glafic.set_lens(3, 'sers', 0.261343256161012, 1e+10, 20.8512217, 20.8340052, 0.58185, 80.9088, 5.97926, 0.5737)
 glafic.set_lens(4, 'pert', 0.261343256161012, 1.0, 2.088982e+01, 2.063720e+01, 1.947641e-02, 1.439174e+02, 0.000000e+00, 0.0)
 glafic.set_point(1, 1.0, 2.084387e+01, 2.073100e+01)
 
 glafic.setopt_lens(1, 0, 0, 0, 0, 1, 1, 1, 0)
-glafic.setopt_lens(2, 0, 1, 0, 0, 0, 0, 0, 0)
-glafic.setopt_lens(3, 0, 1, 0, 0, 0, 0, 0, 0)
+glafic.setopt_lens(2, 0, 1, 1, 1, 0, 0, 0, 0)
+glafic.setopt_lens(3, 0, 1, 1, 1, 0, 0, 0, 0)
 glafic.setopt_lens(4, 0, 0, 1, 1, 1, 1, 0, 0)
 glafic.setopt_point(1, 0, 1, 1)
 
@@ -27,6 +27,7 @@ glafic.setopt_point(1, 0, 1, 1)
 glafic.model_init(verb = 0)
 
 glafic.readobs_point('/Users/ainsleylewis/Documents/Astronomy/IllustrisTNG Lens Modelling/obs_point/obs_point_(POS).dat')
+glafic.parprior("/Users/ainsleylewis/Documents/Astronomy/IllustrisTNG Lens Modelling/priorfile.dat")
 glafic.optimize()
 glafic.findimg()
 glafic.writecrit(1.0)
