dadeccc92449ca3a656b780907ebed9e3120a3e2
rommuluslewis <rommuluslewis@gmail.com>
2025-07-09 00:20:55 +0800

    added mpole crit files

--- Test/kappa_common.py
+++ Test/kappa_common.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import glafic
 
-glafic.init(0.3, 0.7, -1.0, 0.7, "Test/SIE+SHEAR/SIE_POS_SHEAR_macro", 20.0, 20.0, 21.56, 21.56, 0.01, 0.01, 1, verb = 0)
+glafic.init(0.3, 0.7, -1.0, 0.7, "Test/SIE+SHEAR+FIXED/SIE_POS_SHEAR_macro", 20.0, 20.0, 21.56, 21.56, 0.01, 0.01, 1, verb = 0)
 
 glafic.set_secondary('chi2_splane 1', verb = 0)
 glafic.set_secondary('chi2_checknimg 0', verb = 0)
@@ -11,9 +11,9 @@ glafic.set_secondary('hvary          0', verb = 0)
 glafic.set_secondary('ran_seed -122000', verb = 0)
 
 glafic.startup_setnum(2, 0, 1)
-glafic.set_lens(1, "sie", 0.261343256161012, 145.2816, 20.81251, 20.75863, 0.1478786, 13.14078, 0.0, 0.0)
-glafic.set_lens(2, "pert", 0.261343256161012, 1.0, 20.78, 20.78, 0.02624309, 132.1813, 0.0, 0.1544936)
-glafic.set_point(1, 1.000, 20.80957, 20.78065)
+glafic.set_lens(1, "sie", 0.261343256161012, 158.0026, 20.81371, 20.78384, 0.1862623, 74.13216, 0.0, 0.0)
+glafic.set_lens(2, "pert", 0.261343256161012, 1.0, 20.78, 20.78, 0.06086426, -17.08111, 0.0, 0.0)
+glafic.set_point(1, 1.000, 20.81165, 20.78101)
 
 glafic.setopt_lens(1, 0, 0, 0, 0, 0, 0, 0, 0)
 glafic.setopt_lens(2, 0, 0, 0, 0, 0, 0, 0, 0)
