8d21238865a04262e306e3735df64d89fb2dc8ad
rommuluslewis <rommuluslewis@gmail.com>
2025-07-14 23:21:14 +0800

    added new arrows to quiver plots

--- Test/kappa.py
+++ Test/kappa.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import glafic
 
-glafic.init(0.3, 0.7, -1.0, 0.7, "Test/SIE+SHEAR/pert", 20.0, 20.0, 21.56, 21.56, 0.01, 0.01, 1, verb = 0)
+glafic.init(0.3, 0.7, -1.0, 0.7, "Test/POS+MAG/SIE+SHEAR/pert", 20.0, 20.0, 21.56, 21.56, 0.01, 0.01, 1, verb = 0)
 
 glafic.set_secondary('chi2_splane 1', verb = 0)
 glafic.set_secondary('chi2_checknimg 0', verb = 0)
@@ -11,8 +11,8 @@ glafic.set_secondary('hvary          0', verb = 0)
 glafic.set_secondary('ran_seed -122000', verb = 0)
 
 glafic.startup_setnum(1, 0, 1)
-glafic.set_lens(1, "pert", 0.261343256161012, 1.0, 20.78, 20.78, 0.02624309, 132.1813, 0.0, 0.1544936)
-glafic.set_point(1, 1.000, 20.80957, 20.78065)
+glafic.set_lens(1, "pert", 0.261343256161012, 1.0, 20.78, 20.78, 0.02485792, 170.5445, 0.0, 0.1356432)
+glafic.set_point(1, 1.000, 20.80986, 20.77816)
 
 glafic.setopt_lens(1, 0, 0, 1, 1, 1, 1, 1, 0)
 glafic.setopt_point(1, 0, 1, 1)
