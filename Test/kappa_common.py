e92d5a81c6438fa49ae2c2a62328cce4281fa96e
rommuluslewis <rommuluslewis@gmail.com>
2025-07-14 23:21:14 +0800

    added new arrows to quiver plots

--- Test/kappa_common.py
+++ Test/kappa_common.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import glafic
 
-glafic.init(0.3, 0.7, -1.0, 0.7, "Test/SIE+SHEAR/SIE_POS_SHEAR_macro", 20.0, 20.0, 21.56, 21.56, 0.01, 0.01, 1, verb = 0)
+glafic.init(0.3, 0.7, -1.0, 0.7, "Test/POS+MAG/SIE+SHEAR/SIE_POS_SHEAR_macro", 20.0, 20.0, 21.56, 21.56, 0.01, 0.01, 1, verb = 0)
 
 glafic.set_secondary('chi2_splane 1', verb = 0)
 glafic.set_secondary('chi2_checknimg 0', verb = 0)
@@ -11,9 +11,9 @@ glafic.set_secondary('hvary          0', verb = 0)
 glafic.set_secondary('ran_seed -122000', verb = 0)
 
 glafic.startup_setnum(2, 0, 1)
-glafic.set_lens(1, "sie", 0.261343256161012, 145.2816, 20.81251, 20.75863, 0.1478786, 13.14078, 0.0, 0.0)
-glafic.set_lens(2, "pert", 0.261343256161012, 1.0, 20.78, 20.78, 0.02624309, 132.1813, 0.0, 0.1544936)
-glafic.set_point(1, 1.000, 20.80957, 20.78065)
+glafic.set_lens(1, "sie", 0.261343256161012, 146.4924, 20.81547, 20.77724, 0.07993487, 80.27386, 0.0, 0.0)
+glafic.set_lens(2, "pert", 0.261343256161012, 1.0, 20.78, 20.78, 0.02485792, 170.5445, 0.0, 0.1356432)
+glafic.set_point(1, 1.000, 20.80986, 20.77816)
 
 glafic.setopt_lens(1, 0, 0, 0, 0, 0, 0, 0, 0)
 glafic.setopt_lens(2, 0, 0, 0, 0, 0, 0, 0, 0)
