8d3c1062a92e9c94a78f1ad0b2e8f01c288d5b5d
rommuluslewis <rommuluslewis@gmail.com>
2025-07-15 01:24:58 +0800

    added separate mpole_loop file

--- MPOLE/SIE+MPOLE/pos_point.py
+++ MPOLE/SIE+MPOLE/pos_point.py
@@ -1,37 +1,31 @@
 #!/usr/bin/env python
 import glafic
 
-m = [1, 2, 3, 4, 5]
-n = [1, 2, 3, 4, 5]
+glafic.init(0.3, 0.7, -1.0, 0.7, 'MPOLE/SIE+MPOLE/SIE_POS_MPOLE', 20.0, 20.0, 21.56, 21.56, 0.01, 0.01, 1, verb = 0)
 
-for i in range(len(m)):
-    for j in range(len(n)):
+glafic.set_secondary('chi2_splane 1', verb = 0)
+glafic.set_secondary('chi2_checknimg 0', verb = 0)
+glafic.set_secondary('chi2_restart   -1', verb = 0)
+glafic.set_secondary('chi2_usemag    1', verb = 0)
+glafic.set_secondary('hvary          0', verb = 0)
+glafic.set_secondary('ran_seed -122000', verb = 0)
 
-        glafic.init(0.3, 0.7, -1.0, 0.7, f'MPOLE/SIE+MPOLE/SIE_POS_MPOLE_{m[i]}_{n[j]}', 20.0, 20.0, 21.56, 21.56, 0.001, 0.001, 1, verb = 0)
+glafic.startup_setnum(2, 0, 1)
+glafic.set_lens(1, 'sie', 0.261343256161012, 1.58e+02, 20.80, 20.75, 0.12, -4.0, 0.0, 0.0)
+glafic.set_lens(2, 'mpole', 0.261343256161012, 1.0, 20.80, 20.75, 0.01, -90.0, 1, 3)
+glafic.set_point(1, 1.0, 20.78, 20.78)
 
-        glafic.set_secondary('chi2_splane 1', verb = 0)
-        glafic.set_secondary('chi2_checknimg 0', verb = 0)
-        glafic.set_secondary('chi2_restart   -1', verb = 0)
-        glafic.set_secondary('chi2_usemag    1', verb = 0)
-        glafic.set_secondary('hvary          0', verb = 0)
-        glafic.set_secondary('ran_seed -122000', verb = 0)
+glafic.setopt_lens(1, 0, 1, 1, 1, 1, 1, 0, 0)
+glafic.setopt_lens(2, 0, 0, 1, 1, 1, 1, 0, 0)
+glafic.setopt_point(1, 0, 1, 1)
 
-        glafic.startup_setnum(2, 0, 1)
-        glafic.set_lens(1, 'sie', 0.261343256161012, 1.58e+02, 20.80, 20.75, 0.12, -4.0, 0.0, 0.0)
-        glafic.set_lens(2, 'mpole', 0.261343256161012, 1.0, 20.80, 20.75, 0.1, -90.0, m[i], n[j])
-        glafic.set_point(1, 1.0, 20.78, 20.78)
+# model_init needs to be done again whenever model parameters are changed
+glafic.model_init(verb = 0)
 
-        glafic.setopt_lens(1, 0, 1, 1, 1, 1, 1, 0, 0)
-        glafic.setopt_lens(2, 0, 0, 1, 1, 1, 1, 0, 0)
-        glafic.setopt_point(1, 0, 1, 1)
+glafic.readobs_point('/Users/ainsleylewis/Documents/Astronomy/IllustrisTNG Lens Modelling/obs_point/obs_point_(POS).dat')
+glafic.optimize()
+glafic.findimg()
+glafic.writecrit(1.0)
+glafic.writelens(1.0)
 
-        # model_init needs to be done again whenever model parameters are changed
-        glafic.model_init(verb = 0)
-
-        glafic.readobs_point('/Users/ainsleylewis/Documents/Astronomy/IllustrisTNG Lens Modelling/obs_point/obs_point_(POS).dat')
-        glafic.optimize()
-        glafic.findimg()
-        glafic.writecrit(1.0)
-        glafic.writelens(1.0)
-
-        glafic.quit()
+glafic.quit()
