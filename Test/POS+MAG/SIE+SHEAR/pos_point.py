69346b163b5e86282dcb71b4fee72224c11273cf
rommuluslewis <rommuluslewis@gmail.com>
2025-07-14 18:56:20 +0800

    changed test/pos+mag files

--- Test/POS+MAG/SIE+SHEAR/pos_point.py
+++ Test/POS+MAG/SIE+SHEAR/pos_point.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import glafic
 
-glafic.init(0.3, 0.7, -1.0, 0.7, 'Test/SIE+SHEAR/SIE_POS_SHEAR', 20.0, 20.0, 21.56, 21.56, 0.001, 0.001, 1, verb = 0)
+glafic.init(0.3, 0.7, -1.0, 0.7, 'Test/POS+MAG/SIE+SHEAR/SIE_POS_SHEAR', 20.0, 20.0, 21.56, 21.56, 0.001, 0.001, 1, verb = 0)
 
 glafic.set_secondary('chi2_splane 1', verb = 0)
 glafic.set_secondary('chi2_checknimg 0', verb = 0)
@@ -11,9 +11,9 @@ glafic.set_secondary('hvary          0', verb = 0)
 glafic.set_secondary('ran_seed -122000', verb = 0)
 
 glafic.startup_setnum(2, 0, 1)
-glafic.set_lens(1, 'sie', 0.261343256161012, 1.549839e+02, 20.78, 20.78, 0.107, 23.38,  0.000000e+00,  0.000000e+00)
-glafic.set_lens(2, 'pert', 0.261343256161012, 1.0, 20.78, 20.78, 0.1, 140.0, 0.0, 0.2)
-glafic.set_point(1, 1.0, 20.78, 20.78)
+glafic.set_lens(1, 'sie', 0.261343256161012, 1.580486e+02, 2.080967e+01, 2.075980e+01, 1.212350e-01, -4.434456e+00,  0.000000e+00,  0.000000e+00)
+glafic.set_lens(2, 'pert', 0.261343256161012, 1.0, 20.78, 20.78, 0.1, 140.0, 0.0, 0.6)
+glafic.set_point(1, 1.0, 2.081257e+01, 2.077950e+01)
 
 glafic.setopt_lens(1, 0, 1, 1, 1, 1, 1, 0, 0)
 glafic.setopt_lens(2, 0, 0, 0, 0, 1, 1, 0, 1)
@@ -22,7 +22,7 @@ glafic.setopt_point(1, 0, 1, 1)
 # model_init needs to be done again whenever model parameters are changed
 glafic.model_init(verb = 0)
 
-glafic.readobs_point('/Users/ainsleylewis/Documents/Astronomy/IllustrisTNG Lens Modelling/obs_point/obs_point_(POS).dat')
+glafic.readobs_point('/Users/ainsleylewis/Documents/Astronomy/IllustrisTNG Lens Modelling/obs_point/obs_point_(POS+FLUX).dat')
 glafic.optimize()
 glafic.findimg()
 glafic.writecrit(1.0)
