871c8b697817c573358a68da8ccdc6cc1be237a2
rommuluslewis <rommuluslewis@gmail.com>
2025-07-14 18:56:20 +0800

    changed test/pos+mag files

--- Test/POS+MAG/SIE+SHEAR+FIXED/pos_point.py
+++ Test/POS+MAG/SIE+SHEAR+FIXED/pos_point.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import glafic
 
-glafic.init(0.3, 0.7, -1.0, 0.7, 'Test/SIE+SHEAR+FIXED/SIE_POS_SHEAR', 20.0, 20.0, 21.56, 21.56, 0.001, 0.001, 1, verb = 0)
+glafic.init(0.3, 0.7, -1.0, 0.7, 'Test/POS+MAG/SIE+SHEAR+FIXED/SIE_POS_SHEAR', 20.0, 20.0, 21.56, 21.56, 0.001, 0.001, 1, verb = 0)
 
 glafic.set_secondary('chi2_splane 1', verb = 0)
 glafic.set_secondary('chi2_checknimg 0', verb = 0)
@@ -17,12 +17,12 @@ glafic.set_point(1, 1.0, 20.78, 20.78)
 
 glafic.setopt_lens(1, 0, 1, 1, 1, 1, 1, 0, 0)
 glafic.setopt_lens(2, 0, 0, 0, 0, 1, 1, 0, 0)
-glafic.setopt_point(1, 0, 1, 1)
 
+glafic.setopt_point(1, 0, 1, 1)
 # model_init needs to be done again whenever model parameters are changed
 glafic.model_init(verb = 0)
 
-glafic.readobs_point('/Users/ainsleylewis/Documents/Astronomy/IllustrisTNG Lens Modelling/obs_point/obs_point_(POS).dat')
+glafic.readobs_point('/Users/ainsleylewis/Documents/Astronomy/IllustrisTNG Lens Modelling/obs_point/obs_point_(POS+FLUX).dat')
 glafic.optimize()
 glafic.findimg()
 glafic.writecrit(1.0)
