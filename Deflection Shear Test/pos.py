942e141135da004a0f5f26c28efe263eec28116f
rommuluslewis <rommuluslewis@gmail.com>
2025-07-18 17:45:14 +0800

    deflection shear test folder created and py file added

---
+++ Deflection Shear Test/pos.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import glafic
+
+glafic.init(0.3, 0.7, -1.0, 0.7, 'Deflection Shear Test/SHEAR_FIXED_1', 20.0, 20.0, 21.56, 21.56, 0.001, 0.001, 1, verb = 0)
+
+glafic.set_secondary('chi2_splane 1', verb = 0)
+glafic.set_secondary('chi2_checknimg 0', verb = 0)
+glafic.set_secondary('chi2_restart   -1', verb = 0)
+glafic.set_secondary('chi2_usemag    1', verb = 0)
+glafic.set_secondary('hvary          0', verb = 0)
+glafic.set_secondary('ran_seed -122000', verb = 0)
+
+glafic.startup_setnum(1, 0, 1)
+glafic.set_lens(1, 'pert', 0.261343256161012, 1.0, 20.78, 20.78, 0.1, 0.0, 0.0, 0.0)
+glafic.set_point(1, 1.0, 20.78, 20.78)
+
+glafic.setopt_lens(1, 0, 0, 0, 0, 0, 0, 0, 0)
+glafic.setopt_point(1, 0, 0, 0)
+
+# model_init needs to be done again whenever model parameters are changed
+glafic.model_init(verb = 0)
+
+glafic.readobs_point('/Users/ainsleylewis/Documents/Astronomy/IllustrisTNG Lens Modelling/obs_point/obs_point_(POS).dat')
+glafic.writelens(1.0)
+
+glafic.quit()
