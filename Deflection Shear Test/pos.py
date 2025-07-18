9f6be15c0141f58f26cf88c7c6b08993358827a2
rommuluslewis <rommuluslewis@gmail.com>
2025-07-18 18:25:34 +0800

    added saved images of deflection quiver plots

--- Deflection Shear Test/pos.py
+++ Deflection Shear Test/pos.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import glafic
 
-glafic.init(0.3, 0.7, -1.0, 0.7, 'Deflection Shear Test/SHEAR_FIXED_1', 20.0, 20.0, 21.56, 21.56, 0.001, 0.001, 1, verb = 0)
+glafic.init(0.3, 0.7, -1.0, 0.7, 'Deflection Shear Test/SHEAR_FREE_1', 20.0, 20.0, 21.56, 21.56, 0.001, 0.001, 1, verb = 0)
 
 glafic.set_secondary('chi2_splane 1', verb = 0)
 glafic.set_secondary('chi2_checknimg 0', verb = 0)
@@ -11,7 +11,7 @@ glafic.set_secondary('hvary          0', verb = 0)
 glafic.set_secondary('ran_seed -122000', verb = 0)
 
 glafic.startup_setnum(1, 0, 1)
-glafic.set_lens(1, 'pert', 0.261343256161012, 1.0, 20.78, 20.78, 0.1, 0.0, 0.0, 0.0)
+glafic.set_lens(1, 'pert', 0.261343256161012, 1.0, 20.78, 20.78, 0.1, 0.0, 0.0, 0.05)
 glafic.set_point(1, 1.0, 20.78, 20.78)
 
 glafic.setopt_lens(1, 0, 0, 0, 0, 0, 0, 0, 0)
