f97b8d7620c43abbccec7c56aa71c433ce5ec864
rommuluslewis <rommuluslewis@gmail.com>
2025-10-02 11:04:33 +0800

    SIE+SHEAR quiver plot run

--- Test/SIE/pos_point.py
+++ Test/SIE/pos_point.py
@@ -11,8 +11,9 @@ glafic.set_secondary('hvary          0', verb = 0)
 glafic.set_secondary('ran_seed -122000', verb = 0)
 
 glafic.startup_setnum(1, 0, 1)
-glafic.set_lens(1, 'sie', 0.261343256161012, 1.452816e+02, 2.081251e+01, 2.075863e+01, 1.478786e-01, 1.314078e+01, 0.000000e+00, 0.000000e+00)
-glafic.set_point(1, 1.0, 2.080957e+01, 2.078065e+01)
+glafic.set_lens(1, 'sie', 0.261343256161012, 1.008777e+02, 2.081274e+01, 2.076059e+01, 1.310926e-01, 1.621243e+01, 0.000000e+00, 0.000000e+00)
+glafic.set_point(1, 1.0, 2.079421e+01, 2.078032e+01)
+
 glafic.setopt_lens(1, 0, 0, 0, 0, 0, 0, 0, 0)
 glafic.setopt_point(1, 0, 0, 0)
 
