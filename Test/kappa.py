569aeafc51524a5ee7ba54f840d254a18bf750b2
rommuluslewis <rommuluslewis@gmail.com>
2025-10-02 12:14:49 +0800

    plot runs

--- Test/kappa.py
+++ Test/kappa.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import glafic
 
-glafic.init(0.3, 0.7, -1.0, 0.7, "Test/SIE+SHEAR/pert", 20.0, 20.0, 21.56, 21.56, 0.01, 0.01, 1, verb = 0)
+glafic.init(0.3, 0.7, -1.0, 0.7, "Test/SIE+SHEAR+FIXED/pert", 20.0, 20.0, 21.56, 21.56, 0.01, 0.01, 1, verb = 0)
 
 glafic.set_secondary('chi2_splane 1', verb = 0)
 glafic.set_secondary('chi2_checknimg 0', verb = 0)
@@ -11,11 +11,11 @@ glafic.set_secondary('hvary          0', verb = 0)
 glafic.set_secondary('ran_seed -122000', verb = 0)
 
 glafic.startup_setnum(1, 0, 1)
-glafic.set_lens(1, "pert", 0.261343256161012, 1.0, 20.78, 20.78, 0.02940824, 153.0648, 0.0, 0.3901734)
-glafic.set_point(1, 1.000, 20.80094, 20.78059)
+glafic.set_lens(1, "pert", 0.261343256161012, 1.0, 20.78, 20.78, 0.06086426, -17.08111, 0.0, 0.0)
+glafic.set_point(1, 1.000, 20.81165, 20.78101)
 
-glafic.setopt_lens(1, 0, 0, 1, 1, 1, 1, 1, 0)
-glafic.setopt_point(1, 0, 1, 1)
+glafic.setopt_lens(1, 0, 0, 0, 0, 0, 0, 0, 0)
+glafic.setopt_point(1, 0, 0, 0)
 
 # model_init needs to be done again whenever model parameters are changed
 glafic.model_init(verb = 0)
