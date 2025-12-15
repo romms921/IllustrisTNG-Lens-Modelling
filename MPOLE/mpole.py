38de3eb40e5b2cb94b09236614bb6a9605009437
rommuluslewis <rommuluslewis@gmail.com>
2025-12-15 23:12:05 +0800

    multipole run

--- MPOLE/mpole.py
+++ MPOLE/mpole.py
@@ -6,7 +6,7 @@ n = [1, 2, 3, 4, 5]
 
 for i in range(len(m)):
     for j in range(len(n)):
-        glafic.init(0.3, 0.7, -1.0, 0.7, f'MPOLE/sie_{m[i]}_{n[j]}', 20.0, 20.0, 21.56, 21.56, 0.001, 0.001, 1, verb = 0)
+        glafic.init(0.3, 0.7, -1.0, 0.7, f'MPOLE/mpole_{m[i]}_{n[j]}', 20.0, 20.0, 21.56, 21.56, 0.001, 0.001, 1, verb = 0)
 
         glafic.set_secondary('chi2_splane 1', verb = 0)
         glafic.set_secondary('chi2_checknimg 0', verb = 0)
@@ -15,13 +15,13 @@ for i in range(len(m)):
         glafic.set_secondary('hvary          0', verb = 0)
         glafic.set_secondary('ran_seed -122000', verb = 0)
 
-        glafic.startup_setnum(2, 0, 1)
+        glafic.startup_setnum(1, 0, 1)
         glafic.set_lens(1, 'mpole', 0.261343256161012, 1.0, 20.78, 20.78, 0.1, 0.0, m[i], n[j])
-        glafic.set_lens(2, 'sie', 0.261343256161012, 130.0, 20.78, 20.78, 0.5, 0.0, 0.0, 0.0)
+        # glafic.set_lens(2, 'sie', 0.261343256161012, 130.0, 20.78, 20.78, 0.5, 0.0, 0.0, 0.0)
         glafic.set_point(1, 1.0, 20.78, 20.78)
 
         glafic.setopt_lens(1, 0, 0, 0, 0, 0, 0, 0, 0)
-        glafic.setopt_lens(2, 0, 0, 0, 0, 0, 0, 0, 0)
+        # glafic.setopt_lens(2, 0, 0, 0, 0, 0, 0, 0, 0)
         glafic.setopt_point(1, 0, 0, 0)
 
         # model_init needs to be done again whenever model parameters are changed
